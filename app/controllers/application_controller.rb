611d02deec31a79fa9c6f093c60e91dc1c87b2f7
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-22 23:01:48 +0000

    Add helpers, login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,31 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect "/tweets"
+    end
     erb :signup
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  get '/tweets' do
+    @tweets = Tweet.all
+    erb :tweets
+  end
+
+
+  post '/login' do
+
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    else
+      erb :error
+    end
   end
 
   post '/signup' do
@@ -23,10 +47,10 @@ class ApplicationController < Sinatra::Base
       user.email = params[:email]
       user.password = params[:password]
       user.save
+      session[:user_id] = user.id
       redirect '/tweets'
     end
     redirect '/signup'
-
   end
 
 end
