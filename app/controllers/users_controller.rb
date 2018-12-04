ca7865d4e1d4e1db7c3158dc27c07fa77fc5bfcf
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-04 16:33:52 -0500

    generate controllers from generator

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,54 @@
-class UsersController < ApplicationController
+class UserController < ApplicationController
 
+  # Index Action
+  get '/users' do
+    @users = User.all
+    erb :'/users/index'
+  end
+  
+  # New Action
+  get '/users/new' do
+    @tweets = Tweet.all
+    erb :'/users/new'
+  end
+  
+  # Create Action
+  post '/users' do
+    user = User.create(params['user'])
 
+    user.tweets << Tweet.create(name: params['tweet_name']) unless params['tweet_name'].empty?
+    user.save
+    redirect "users/#{user.id}"
+  end
+  
+  # Show Action
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :'/users/show'
+  end
+  
+  # Edit Action
+  get '/users/:id/edit' do
+    @user = User.find(params[:id])
+    @tweets = Tweet.all
+    erb :'/users/edit'
+  end
+  
+  # Patch Action
+  patch '/users/:id' do
+    params[:song_info]['tweet_ids'].clear if !params[:user].keys.include?('tweet_ids')
+    user = User.find(params[:id])
+    user.update(params['user'])
+
+    user.tweets << Tweet.create(name: params['tweet_name']) unless params['tweet_name'].empty?
+    user.save
+    redirect "users/#{user.id}"
+  end
+  
+  # Delete Action
+  delete '/users/:id' do
+    user = User.find(params[:id])
+    user.delete
+  end
+  
 end
