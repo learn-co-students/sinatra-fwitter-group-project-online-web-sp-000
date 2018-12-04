2a4e488cddbc065dc1261141fd917646d53895f7
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-04 16:46:09 -0500

    import slugifiable, pass user model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
+
+  require_relative './concerns/slugifiable'
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+
 end
