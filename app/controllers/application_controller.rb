4768178e28d4fede49493399394264c14efccf7f
kaelaclement <stephanie.kaela@gmail.com>
2018-12-13 22:52:43 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,12 +14,12 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
-    def current_user(session)
-      User.find(session[:id])
+    def current_user
+      User.find(session[:user_id])
     end
 
     def logged_in?
-      !!session[:id]
+      !!session[:user_id]
     end
   end
 
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,72 @@
 class TweetsController < ApplicationController
+  get '/tweets' do
+    if logged_in?
+      erb :'/tweets/index'
+    else
+      redirect '/login'
+    end
+  end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    if !params[:content].empty?
+      tweet = Tweet.create(user: current_user, content: params[:content])
+
+      redirect "/tweets/#{tweet.id}"
+    else
+      redirect '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+
+      erb :'/tweets/show'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+
+      erb :'/tweets/edit'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    tweet = Tweet.find(params[:id])
+
+    if params[:content].empty?
+      redirect "/tweets/#{tweet.id}/edit"
+    elsif logged_in? && current_user == tweet.user
+      tweet.update(content: params[:content])
+      redirect "/tweets/#{tweet.id}"
+    else
+      redirect '/login'
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    tweet = Tweet.find(params[:id])
+
+    if logged_in? && current_user == tweet.user
+      tweet.delete
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
 
 end
