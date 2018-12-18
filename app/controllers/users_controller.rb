337445807fea3d3ed8a9d036523ebb8c8ded7522
Douglas Uretsky <douglasuretsky@gmail.com>
2018-12-18 17:56:45 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,11 @@ class UsersController < ApplicationController
   get '/signup' do
     erb :'users/signup'
   end
+  
+  post '/signup' do
+    @user = User.create(params)
+    # binding.pry
+    redirect :'/tweets'
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :tweets
+  validates_presence_of :username
   has_secure_password
 end
