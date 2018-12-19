dc592d55a473919a53fcbd311492f6e43bdaa2e4
Douglas Uretsky <douglasuretsky@gmail.com>
2018-12-19 15:29:49 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,5 +8,39 @@ class TweetsController < ApplicationController
       redirect :'login'
     end
   end
-
+  
+  post '/tweets' do
+    if logged_in?
+      if params[:content] == ""
+        redirect :'/tweets/new'
+      end
+      @tweet = Tweet.create(params)
+      current_user.tweets << @tweet
+    else
+      redirect :'/login'
+    end
+  end
+  
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+  
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/new'
+    else
+      redirect to '/login'
+    end
+  end
+  
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+    end
+  end
 end
