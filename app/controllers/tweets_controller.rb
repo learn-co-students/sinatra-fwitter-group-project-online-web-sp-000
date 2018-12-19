b5e2d68287d503e9d67fe0b9b109bcbc70a7eb20
Douglas Uretsky <douglasuretsky@gmail.com>
2018-12-19 15:43:52 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -21,26 +21,27 @@ class TweetsController < ApplicationController
     end
   end
   
-  get '/tweets/:id' do
-    if logged_in?
-      @tweet = Tweet.find_by_id(params[:id])
-      erb :'tweets/show_tweet'
-    else
-      redirect to '/login'
-    end
-  end
   
   get '/tweets/new' do
     if logged_in?
       erb :'tweets/new'
     else
-      redirect to '/login'
+      redirect :'/login'
     end
   end
   
   get '/tweets/:id/edit' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
+    end
+  end
+  
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect :'/login'
     end
   end
 end
