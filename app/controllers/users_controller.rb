cbf3d661049cb9add5df8dcc19cf8084e73cb45d
aburk3 <aburk3@gmail.com>
2018-12-29 14:32:57 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    @user.save
+    redirect to '/tweets'
+  end
 
 end
