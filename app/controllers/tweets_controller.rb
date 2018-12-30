40651271a88304353276b4a668951c0a28d177c4
aburk3 <aburk3@gmail.com>
2018-12-30 17:31:18 -0600

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -34,5 +34,14 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
 
 end
