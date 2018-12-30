6ca654d0f89bced072b9a6c546bbc1df2414a95f
aburk3 <aburk3@gmail.com>
2018-12-29 21:50:15 -0600

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,12 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-    @tweets = Tweet.all
-    erb :'tweets/tweets'
+    if is_logged_in?
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect to '/login'
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,32 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-    @user.save
-    session[:user_id] = @user.id
-    
-    redirect to '/tweets'
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    end
   end
+
+  get '/login' do
+    if !is_logged_in?
+      erb :'users/login'
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+   if user && user.authenticate(params[:password])
+     session[:user_id] = user.id
+     redirect "/tweets"
+   else
+     redirect to '/signup'
+   end
+ end
 
 end
