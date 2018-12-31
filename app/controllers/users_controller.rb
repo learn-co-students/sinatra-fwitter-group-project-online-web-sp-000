92d593e9a0dd042ee425c69b252c214e8a1250b6
aburk3 <aburk3@gmail.com>
2018-12-30 18:06:58 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+     @user = User.find_by_slug(params[:slug])
+     erb :'users/show'
+   end
+
   get '/signup' do
      if !logged_in?
        erb :'users/create_user'
