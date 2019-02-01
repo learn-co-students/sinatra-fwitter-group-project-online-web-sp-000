e73b9c4303d9904753e92fe4fcc1cbd0d89fbf9c
Aaron Berns <anberns@gmail.com>
2019-02-01 12:02:42 -0500

    implement index, signup routes and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
 
+  #load signup form
+  get '/signup' do 
+    if session[:user_id]
+      redirect to '/tweets'
+    else
+      erb :'user/signup'
+    end
+  end
 
+  post '/signup' do 
+    if params["username"] == "" || params["email"] == "" || params["password"] == ""
+      redirect '/signup'
+    else
+      @user = User.create(username: params["username"], email: params["email"], password: params["password"])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    end
+  end
 end
