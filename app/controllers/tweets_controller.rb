532574d26393b63f801d8f70d75bb5ce175064e3
Nicole D <nnardella@gmail.com>
2019-02-24 02:27:19 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,7 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-
-    erb :tweets
+    erb :'tweets'
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,50 @@
 class UsersController < ApplicationController
 
     get '/login' do
-    if logged_in?
-      redirect '/tweets'
-    else
+      if logged_in?
+        redirect '/tweets'
+      else
       erb :'/users/login'
+      end
     end
-  end
 
-    #
-    # post '/login' do
-    #   user = User.find_by(:username => params[:username])
-    #
-    #    if user && user.authenticate(params[:password]) && user.username != ""
-    #      session[:user_id] = user.id
-    #
-    #       redirect "/users/#{user.id}"
-    #   else
-    #      erb :"users/error"
-    #   end
-    # end
+    post '/login' do
+      user = User.find_by(:username => params[:username])
+
+       if user && user.authenticate(params[:password]) && user.username != ""
+         session[:user_id] = user.id
+
+          redirect "/tweets"
+      else
+         erb :"users/error"
+      end
+    end
 
     get '/signup' do
-    if logged_in?
-      redirect '/tweets'
-    else
+      if logged_in?
+        redirect '/tweets'
+      else
       erb :'/users/create_user'
+      end
     end
-  end
-
 
     post '/signup' do
 
     if params[:username] != "" && params[:email] != "" && params[:password] != ""
   		 @user = User.create(params)
-  		     redirect "/login"
-           #or we could log the user in then redirect to show page
-            # session[:user_id] = user.id
-            # redirect "/users/#{user.id}"
+        session[:user_id] = user.id
+
+        redirect "/tweets"
 
   		   else
   		     redirect "/signup"
   		  end
       end
 
-    get '/users/:id' do
-      "this will be the users show route"
+
+    get '/logout' do
+      session.destroy
+      redirect '/login'
     end
 
 
