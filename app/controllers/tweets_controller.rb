aaecb1d57f0de205318081be57d2f0da205f547a
Nicole D <nnardella@gmail.com>
2019-02-25 15:14:56 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,26 +1,34 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-    if logged_in? && current_user
+    if logged_in?
       @tweets = Tweet.all
     erb :'/tweets/tweets'
+     else
+       redirect '/login'
    end
   end
 
   get '/tweets/new' do
     if logged_in?
       erb :"/tweets/new"
+    else
+      redirect '/login'
     end
   end
 
   post '/tweets' do
     if logged_in? && params[:content] != ""
+      binding.pry
       @tweet = Tweet.new(content: params[:content])
       @tweet.user_id = current_user.id
       @tweet.save
-    end
+
     redirect '/tweets/show'
+   else
+    redirect '/login'
   end
+end
 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,8 +40,12 @@ class UsersController < ApplicationController
     end
 
     get '/logout' do
-      session.clear
-      redirect '/login'
+      if logged_in?
+        session.clear
+        redirect '/login'
+     else
+       redirect '/'
+     end
     end
 
 
