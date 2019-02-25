032888330a74974d9f1f4655ad3714a07f877a99
Nicole D <nnardella@gmail.com>
2019-02-25 01:45:03 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,21 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
+    if logged_in
+      @tweets = Tweet.all
     erb :'/tweets/tweets'
   end
+
+  get '/tweets' do
+
+    erb :'/tweets/new'
+  end
+
+  post '/tweets' do
+
+    redirect '/tweets/:id'
+  end
+
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,6 @@ class UsersController < ApplicationController
       end
     end
 
-
     get '/logout' do
     session.clear
     redirect '/login'
