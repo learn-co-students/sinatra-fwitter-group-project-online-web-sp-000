a907a750ddac6ad06f30361f631a3c32398c7e27
Nicole D <nnardella@gmail.com>
2019-02-25 16:10:55 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -38,13 +38,17 @@ end
     @tweet = Tweet.find(params[:id])
 
     erb :"/tweets/show_tweet"
+    else
+      redirect '/login'
    end
   end
 
   get '/tweets/:id/edit' do
     if logged_in?
-    @tweet = Tweet.find(params[:id])
-    erb :"/tweets/edit_tweet"
+      @tweet = Tweet.find(params[:id])
+      erb :"/tweets/edit_tweet"
+    else
+     redirect '/login'
    end
   end
 
