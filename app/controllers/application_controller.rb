8223581b4a803a865e946bff0cbeb3702beaf178
Rafael Batista <rafaelb24@yahoo.com>
2019-03-07 18:41:47 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,75 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  
+ #  'enable :sessions' was causing the login to loop in the browser even though all tests were passing. The session was getting cleared from one controller action to the other
+ 
+ # 'use Rack::Session::Cookie' fixed the log in loop and all tests still pass.
+ 
+  
+  use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'fwitter_is_awesome'
+  
+  get '/' do 
+    erb :'/homepage'
+  end 
+  
+  get '/signup' do 
+    redirect '/tweets' if logged_in?
+    erb :'/signup'
+  end
+  
+  post '/signup' do 
+    params.each do |k, v|
+      v.empty? ? redirect('/signup') : next
+    end 
+    
+    user = User.create(params)
+    log_in(user)
+    redirect '/tweets'
+  end 
+  
+  get '/tweets' do 
+    redirect '/login' if !logged_in?
+    @user = current_user
+    erb :'/tweets'
+  end 
+  
+  get '/login' do 
+    logged_in? ? redirect('/tweets') : erb(:'/login')
+  end 
+  
+  post '/login' do 
+    user = User.find_by(username: params[:username])
+    if user.authenticate(params[:password])
+      log_in(user)
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end 
+  end 
+  
+  get '/logout' do 
+    logged_in? ? logout : redirect('/login')
+  end 
+  
+  helpers do 
+    def current_user 
+      User.find(session[:user_id])
+    end 
+    
+    def log_in(user) 
+      session[:user_id] = user.id
+    end 
+    
+    def logged_in? 
+      !!session[:user_id]
+    end 
+    
+    def logout 
+      session.clear
+      redirect '/login'
+    end 
+  end 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,57 @@
+require './config/environment'
+
 class TweetsController < ApplicationController
 
-
+  get '/tweets/new' do 
+    logged_in? ? erb(:'/new') : redirect('/login')
+  end
+  
+  post '/tweets' do 
+    @user = current_user
+    params[:content].empty? ? redirect('/tweets/new') : @user.tweets.build(params)
+    @user.save
+    
+    redirect "/tweets/#{Tweet.last.id}"
+  end 
+  
+  get '/tweets/:id' do 
+    if logged_in?
+      @user = current_user
+      @tweet = Tweet.find(params[:id])
+      erb :'/show'
+    else 
+      redirect '/login'
+    end 
+  end 
+  
+  get '/tweets/:id/edit' do 
+    if logged_in?  
+      @tweet = Tweet.find(params[:id])
+      erb :'/edit'
+    else 
+      redirect '/login'
+    end 
+  end 
+  
+  patch '/tweets/:id' do 
+    redirect '/login' if !logged_in?
+    @tweet = Tweet.find(params[:id])
+     if !params[:content].empty?
+        @tweet.content = params[:content] 
+        @tweet.save
+     else 
+        redirect "/tweets/#{@tweet.id}/edit"
+     end
+  end
+  
+  delete '/tweets/:id' do 
+    if logged_in? 
+      user = current_user
+      tweet = Tweet.find(params[:id])
+      user == tweet.user ? tweet.delete : redirect("/login")
+      redirect "/users/#{user.slug}"
+    else 
+      redirect '/login'
+    end
+  end
 end
