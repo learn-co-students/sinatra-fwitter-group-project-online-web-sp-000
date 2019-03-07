f2c212abcbc66a64c623221048298c9c0dcfa8a3
Rafael Batista <rafaelb24@yahoo.com>
2019-03-07 19:10:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'sinatra/base'
+require 'rack-flash'
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -7,7 +9,8 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
   
- #  'enable :sessions' was causing the login to loop in the browser even though all tests were passing. The session was getting cleared from one controller action to the other
+
+ #  'enable :sessions' was causing the login to loop in the browser even though all tests were passing. The session was   getting cleared from one controller action to the other
  
  # 'use Rack::Session::Cookie' fixed the log in loop and all tests still pass.
  
@@ -41,23 +44,7 @@ class ApplicationController < Sinatra::Base
     erb :'/tweets'
   end 
   
-  get '/login' do 
-    logged_in? ? redirect('/tweets') : erb(:'/login')
-  end 
-  
-  post '/login' do 
-    user = User.find_by(username: params[:username])
-    if user.authenticate(params[:password])
-      log_in(user)
-      redirect '/tweets'
-    else
-      redirect '/login'
-    end 
-  end 
-  
-  get '/logout' do 
-    logged_in? ? logout : redirect('/login')
-  end 
+ 
   
   helpers do 
     def current_user 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
     @user = User.find_by_slug(params[:slug])
     erb :'/index'
   end 
+  
+   get '/login' do 
+    logged_in? ? redirect('/tweets') : erb(:'/login')
+  end 
+  
+  post '/login' do 
+    user = User.find_by(username: params[:username])
+    if user.authenticate(params[:password])
+      log_in(user)
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end 
+  end 
+  
+  get '/logout' do 
+    logged_in? ? logout : redirect('/login')
+  end 
 end
