7b800f6dc619f7fbfbb091848879ce0b578202c1
jonmackeyparty <jon@jonmackey.party>
2019-03-13 16:53:47 -0400

    add logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+  get '/users/logout' do
+    binding.pry
+    session.clear
+    erb :'/login'
+  end
+
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     erb :'/users/show'
