a1f4f667a246281543a2fb83bb6caaaf01edc8ac
Diana Badas <diana.badas@gmail.com>
2019-04-16 16:11:59 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,24 @@
 class TweetsController < ApplicationController
+
+  #Index
+
   get '/tweets' do
     @tweets = Tweet.all
 
     erb :'tweets/tweets'
+  end
+
+  #Create
+  get '/tweets/new' do
+    @tweet = Tweet.new
+
+    erb :'tweets/new'
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.create(params)
+
+    redirect to "/tweets/#{ @tweet.id}"
   end
 
 end
