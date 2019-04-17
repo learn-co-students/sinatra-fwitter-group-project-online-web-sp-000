af8069386c89d2f842b5572b5bea5f11d8c05a36
Diana Badas <diana.badas@gmail.com>
2019-04-17 13:07:40 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-   
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
   #SignUp
   get '/signup' do
     if !logged_in?
