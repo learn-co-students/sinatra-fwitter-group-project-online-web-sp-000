c74db7541f4abafd6c32b5d42848175bc0c25b0a
Robert Tate <btate712@gmail.com>
2019-04-27 21:03:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,49 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'users/new'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] != "" && params[:email] != "" && params[:password] != ""
+      user = User.create(params)
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      @login_failed = true
+      erb :login
+    end
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,40 @@
 class TweetsController < ApplicationController
+  get '/tweets' do
+    @user = User.find(session[:user_id])
 
+    erb :'tweets/index'
+  end
 
+  get '/tweets/new' do
+
+    erb :'tweets/new'
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+
+    erb :'tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+
+    erb :'tweets/edit'
+  end
+
+  post '/tweets' do
+    erb :'tweets/index'
+  end
+
+  post '/tweets/:id/delete' do
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  patch '/tweets' do
+    tweet = Tweet.find(params[:id])
+    tweet.content = params[:tweet][:content]
+
+    redirect "/tweets/#{tweet.id}"
+  end
 end
