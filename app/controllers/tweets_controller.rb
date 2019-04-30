ab7beb94b08f00364a7f44bba0ec535b3c5d7cc6
Andrew Waller <andrewhwaller@gmail.com>
2019-04-30 03:02:49 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,14 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    erb :"/tweets/index.erb"
+  end
 
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
+
+  post '/tweets' do
+
+  end
 end
