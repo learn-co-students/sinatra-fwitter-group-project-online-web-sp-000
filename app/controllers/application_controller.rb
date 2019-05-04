cdd3844286f227decea635b183b360eeef95d28f
Mackenzie Moore <macmoore@ucdavis.edu>
2019-05-04 00:11:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,54 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    #binding.pry
+    if valid_signup?
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/tweets' do
+    erb :'tweets/index'
+  end
+
+  get '/tweets/new' do
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.create(params)
+    erb :'tweets/show'
+  end
+
+  get '/tweets/:id' do
+    erb :'tweets/show'
+  end
+
+
+  helpers do
+    def valid_signup?
+      if params[:username] != "" &&  params[:email] != "" && params[:password] != "" && params[:username] && params[:email] && params[:password]
+        true
+      else
+        false
+      end
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
+
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,2 @@
 class TweetsController < ApplicationController
-
-
 end
