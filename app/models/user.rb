630c4db2b2cf2ce09dd294d1fec6536ebe66cc3d
Mackenzie Moore <macmoore@ucdavis.edu>
2019-05-06 18:53:29 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.detect {|user| user.username.downcase.gsub(" ", "-") == slug}
+  end
 end
