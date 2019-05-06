c803e9d31054f52fc4ef7be9263e8bd2e17eee35
Mackenzie Moore <macmoore@ucdavis.edu>
2019-05-06 22:10:37 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,25 @@
 class TweetsController < ApplicationController
+  get '/tweets' do
+    #binding.pry
+    if logged_in?
+      @user = User.find(session[:user_id])
+      erb :tweets
+    else
+      redirect "/login"
+    end
+  end
+
+
+    get '/tweets/new' do
+    end
+
+    post '/tweets' do
+      @tweet = Tweet.create(params)
+      erb :'tweets/show'
+    end
+
+    get '/tweets/:id' do
+      erb :'tweets/show'
+    end
+    
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,49 @@
 class UsersController < ApplicationController
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+      redirect to '/tweets'
+    else
+      erb :signup
+    end
+  end
 
+  post '/signup' do
+    #binding.pry
+    if valid_signup?
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :login
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/tweets"
+    else
+      redirect "/failure"
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect "/login"
+    else
+      redirect "/"
+    end
+  end
 
 end
