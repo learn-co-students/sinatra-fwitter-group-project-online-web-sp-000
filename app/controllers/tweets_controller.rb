ebe4cfcf07c8884299bec77937d720d0fd6d04bb
J. Singleton <jennifer.singleton@gmail.com>
2019-05-08 03:29:23 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,30 @@
 class TweetsController < ApplicationController
 
+    get '/tweets/new' do
+      erb :new
+    end
 
+    post '/tweets' do
+
+    end
+
+    get '/tweets' do
+      if logged_in?
+        @user = User.find_by(session[:user_id])
+        @tweets = @user.tweets
+        erb :"tweets/tweets"
+      else
+        redirect '/login'
+      end
+    end
+
+    helpers do
+      def logged_in?
+        !!session[:user_id]
+      end
+
+      def current_user
+        User.find(session[:user_id])
+      end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,54 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect to "/tweets"
+    else
+      erb :"users/create_user"
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
+      redirect "/signup"
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: [:password])
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :"users/login"
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
+
+  helpers do
+      def logged_in?
+        !!session[:user_id]
+      end
+
+      def current_user
+        User.find(session[:user_id])
+      end
+    end
 
 end
