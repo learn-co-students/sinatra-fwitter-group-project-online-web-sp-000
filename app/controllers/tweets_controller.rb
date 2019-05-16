8e5a2f0f1ad5ae58d73c31334c45dd2fc42e0bde
Jeff Benton <sikletz@gmail.com>
2019-05-16 21:55:39 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,52 @@
 class TweetsController < ApplicationController
-
-
+  
+  get '/tweets' do
+    redirect "/login" if !Helper.is_logged_in?(session)
+    
+    @user = User.find(session[:user_id])
+    erb :"/tweets/tweets"
+  end
+  
+  get '/tweets/new' do
+    redirect "/login" if !Helper.is_logged_in?(session)
+    
+    erb :'/tweets/new'
+  end
+  
+  post '/tweets' do
+    redirect "/tweets/new" if params[:content].empty?
+    
+    tweet = Tweet.create(content: params[:content], user_id: session[:user_id])
+    redirect "/tweets"
+  end
+  
+  get '/tweets/:id' do
+    redirect "/login" if !Helper.is_logged_in?(session)
+    
+    @tweet = Tweet.find(params[:id])
+    erb :"/tweets/show_tweet"
+  end
+  
+  post '/tweets/:id' do
+    redirect "/tweets/#{params[:id]}/edit" if params[:content].empty?
+    
+    Tweet.update(params[:id], content: params[:content])
+    redirect "/tweets/#{params[:id]}"
+  end
+  
+  get '/tweets/:id/edit' do
+    redirect "/login" if !Helper.is_logged_in?(session)
+    
+    @tweet = Tweet.find(params[:id])
+    erb :"/tweets/edit_tweet"
+  end
+  
+  post '/tweets/:id/delete' do
+    tweet = Tweet.find(params[:id])
+    redirect "/tweets/#{params[:id]}" if tweet.user_id != session[:user_id]
+    
+    tweet.delete
+    redirect "/tweets"
+  end
+  
 end
---
+++ app/helpers/helpers.rb
@@ -0,0 +1,9 @@
+class Helper
+  def self.current_user(session)
+    User.find(session[:user_id])
+  end
+  
+  def self.is_logged_in?(session)
+    session.include?(:user_id)
+  end
+end
