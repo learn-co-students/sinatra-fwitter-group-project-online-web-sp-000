133d516482bcc1333c626a875966e5874d3727fe
davisjustinw <davisjustinw@gmail.com>
2019-05-18 10:24:31 -0700

    Application Controller -> Homepae and Signup page pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,31 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect '/tweets'
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if params.all? { |param| !param.last.empty? }
+      params.delete :submit
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
   end
 
 end
---
+++ app/models/tweet.rb
@@ -0,0 +1,3 @@
+class Tweets < ActiveRecord::Base
+  belongs_to :user
+end
