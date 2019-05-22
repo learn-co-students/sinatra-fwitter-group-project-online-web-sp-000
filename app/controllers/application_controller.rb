8487c3de0809994c069adca3d3faf9a5a7bc5ab4
Joe Ganobsik <jganobsik@gmail.com>
2019-05-22 14:33:54 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,24 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+     enable :sessions
+    set :session_secret, "secret_phrase"
   end
 
+ get '/' do
+      erb :index
+  end
+
+
+private
+
+def logged_in?
+   !!current_user
+end
+
+def current_user
+  if session[:user_id]
+  @current_user ||= User.find_by(id: session[:user_id])
+end
+end
 end
