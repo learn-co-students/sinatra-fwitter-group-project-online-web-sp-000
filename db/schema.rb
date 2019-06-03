64dd5f7a1137cca4685a35dacd4e71d56d2396bb
John Hayes <johnnyhayes7@gmail.com>
2019-06-03 16:41:01 +0000

    tweets table and tweet class built

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190603134753) do
+ActiveRecord::Schema.define(version: 20190603163828) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string  "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
