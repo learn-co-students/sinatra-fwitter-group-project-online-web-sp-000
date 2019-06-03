0a09e2432ca392ce5db129888be28c72ccbd9ee6
John Hayes <johnnyhayes7@gmail.com>
2019-06-03 13:45:08 +0000

    find by slug completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ActiveRecord::Base
 
   def slug
     self.username.split.join("-")
+
   end
 
+  def self.find_by_slug(slug)
+    self.all.find do |u|
+      if u.slug == slug
+        u
+      end
+    end
+  end
 end
