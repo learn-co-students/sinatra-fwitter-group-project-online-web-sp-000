944349b8151e3249777ab864e05b15e349b23838
John Hayes <johnnyhayes7@gmail.com>
2019-06-04 17:19:12 +0000

    completed login

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,12 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-    if !logged_in(session)
+    if !logged_in?(session)
       redirect "/login"
     end
     @user = current_user(session)
     @tweets = Tweet.all
-    # binding.pry
+
     erb :'/tweets/index'
   end
 end
