85538b1af74fbea67eebeac68564f1cb478441ce
John Hayes <johnnyhayes7@gmail.com>
2019-06-05 18:28:40 +0000

    show tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,10 +19,13 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/:id' do
-    if logged_in?(session)
+    if !logged_in?(session)
+      redirect '/login'
+    end
+
     @tweet = Tweet.find_by_id(params[:id])
     erb :'tweets/show'
-    end
+
   end
 
   post '/tweets' do
