b749025dbd684f7f5612d1df39715b4c81834e7d
Barbara Johnson <johnson.barbara@gmail.com>
2019-06-11 00:43:25 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
+require_relative './concerns/slugify.rb'
+
 class User < ActiveRecord::Base
+      
+    #all instances of User have access to Slugifiable's methods (as instance methods)
+    include Slugifiable 
+
+    #User has Slugifiable's methods (as class methods)
+    extend Slugifiable
   has_secure_password
   has_many :tweets
 end
