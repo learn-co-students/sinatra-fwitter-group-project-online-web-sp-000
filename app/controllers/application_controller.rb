188a97f174711975afce9cd82878ffd2d3b6ed26
Jim Hough <teachlife@outlook.com>
2019-07-07 19:49:48 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,53 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, 'password privacy'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    end
+    erb :signup
+  end
+
+  post '/signup' do
+    user = User.new
+    user.username = params[:username]
+    user.email = params[:email]
+    user.password = params[:password]
+    if (params[:username] == "" || params[:email] == "" || params[:password] == "")
+      redirect '/signup'
+    else
+      user.save
+      session[:user_id] = user.id
+      redirect "/tweets"
+    end
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def slug
+    self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+    full_name = nil
+    self.all.each do |obj|
+      if obj.slug == slug
+        full_name = obj
+      end
+    end
+    full_name
+  end
 end
