840ffa8ef226b187bbea65b81b456b0330bcc8e4
Jim Hough <teachlife@outlook.com>
2019-07-08 02:55:48 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class TweetsController < ApplicationController
 
   get '/tweets' do
@@ -7,6 +8,73 @@ class TweetsController < ApplicationController
       erb :'tweets/index'
     else
       redirect '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if logged_in?
+      @user = current_user
+      erb :'tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    if (params[:content] == "")
+      redirect "/tweets/new"
+    else
+      @user = current_user
+      tweet = Tweet.create(:content => params[:content], :user_id => @user.id)
+      redirect '/tweets'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet=Tweet.find(params[:id])
+      @user= current_user
+      if @tweet.user == @user
+        erb :'/tweets/edit'
+      else
+        redirect '/tweets'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      @user = current_user
+      erb :'tweets/show'
+    else
+      redirect '/login'
+    end
+  end
+
+  delete '/tweets/:id' do
+    if logged_in?
+      tweet = Tweet.find(params[:id])
+      if tweet.user == current_user
+        tweet.destroy
+        redirect '/tweets'
+      else
+        redirect '/tweets'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if (params[:content] == '')
+      redirect "/tweets/#{params[:id]}/edit"
+    else
+      tweet=Tweet.find(params[:id])
+      tweet.update(:content => params[:content])
+      redirect "/tweets/#{tweet.id}"
     end
   end
 end
