b77643a9bbfe8600c00261c6ecfc1d98cd2a27ee
Donna Amos <donnacamos.developer@gmail.com>
2019-07-20 00:14:24 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,148 @@
 require './config/environment'
+require 'sinatra/base'
+require 'rack-flash'
+
 
 class ApplicationController < Sinatra::Base
-
+  enable :sessions
+  use Rack::Flash
   configure do
+    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    end
+
+    erb :"/users/create_user"
+  end
+
+  post '/signup' do
+    params.each do |label, input|
+      if input.empty?
+        flash[:new_user_error] = "Please enter a value for #{label}"
+        redirect to '/signup'
+      end
+    end
+
+    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+    session[:user_id] = user.id
+
+    redirect to '/tweets'
+  end
+
+  get '/login' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    end
+
+    erb :"/users/login"
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params["username"])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      flash[:login_error] = "Incorrect login. Please try again."
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets' do
+    if !Helpers.is_logged_in?(session)
+      redirect to '/login'
+    end
+    @tweets = Tweet.all
+    @user = Helpers.current_user(session)
+    erb :"/tweets/tweets"
+  end
+
+  get '/tweets/new' do
+    if !Helpers.is_logged_in?(session)
+      redirect to '/login'
+    end
+    erb :"/tweets/create_tweet"
+  end
+
+  post '/tweets' do
+    user = Helpers.current_user(session)
+    if params["content"].empty?
+      flash[:empty_tweet] = "Please enter content for your tweet"
+      redirect to '/tweets/new'
+    end
+    tweet = Tweet.create(:content => params["content"], :user_id => user.id)
+
+    redirect to '/tweets'
+  end
+
+  get '/tweets/:id' do
+    if !Helpers.is_logged_in?(session)
+      redirect to '/login'
+    end
+    @tweet = Tweet.find(params[:id])
+    erb :"tweets/show_tweet"
+  end
+
+  get '/tweets/:id/edit' do
+    if !Helpers.is_logged_in?(session)
+      redirect to '/login'
+    end
+    @tweet = Tweet.find(params[:id])
+    if Helpers.current_user(session).id != @tweet.user_id
+      flash[:wrong_user_edit] = "Sorry you can only edit your own tweets"
+      redirect to '/tweets'
+    end
+    erb :"tweets/edit_tweet"
+  end
+
+  patch '/tweets/:id' do
+    tweet = Tweet.find(params[:id])
+    if params["content"].empty?
+      flash[:empty_tweet] = "Please enter content for your tweet"
+      redirect to "/tweets/#{params[:id]}/edit"
+    end
+    tweet.update(:content => params["content"])
+    tweet.save
+
+    redirect to "/tweets/#{tweet.id}"
+  end
+
+  post '/tweets/:id/delete' do
+    if !Helpers.is_logged_in?(session)
+      redirect to '/login'
+    end
+    @tweet = Tweet.find(params[:id])
+    if Helpers.current_user(session).id != @tweet.user_id
+      flash[:wrong_user] = "Sorry you can only delete your own tweets"
+      redirect to '/tweets'
+    end
+    @tweet.delete
+    redirect to '/tweets'
+  end
+
+  get '/users/:slug' do
+    slug = params[:slug]
+    @user = User.find_by_slug(slug)
+    erb :"users/show"
+  end
+
+  get '/logout' do
+    if Helpers.is_logged_in?(session)
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
   end
 
 end
