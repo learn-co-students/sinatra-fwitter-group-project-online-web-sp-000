731e381251e83c0c0c38a67dfd7ffaf7c64abe70
Alan Krajina <alankrajina88@gmail.com>
2019-07-24 12:30:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
+
+#  get '/' do
+#  if logged in to tweets else index
+
+
+  # tu ide samo helper methods sa kraja (logged_in?, current user)
 
 end
