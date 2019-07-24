e32b0ca8bea10dd5834967bd6dcbbbb0f12c754f
Alan Krajina <alankrajina88@gmail.com>
2019-07-24 14:37:03 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,43 @@
 class UsersController < ApplicationController
 
+  get "/signup" do
+    erb :'/users/create_user'
+  end
+
+  post "/signup" do
+	@user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+
+	  if @user.save && @user[:username] != ""
+	    erb :"/tweets/tweets"
+    end
+  end
+
+
+
+
+
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :"/tweets/tweets"
+  end
+
+
+  get "/login" do
+    erb :'/users/login'
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+#binding.pry
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+#    else
+#      redirect "/failure"
+    end
+  end
+
+
 
 end
--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password
+      t.string :email
       t.string :password_digest
     end
   end
