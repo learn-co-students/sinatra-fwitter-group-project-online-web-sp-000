0d702b12731197c794a99a7b2cd79f4f7cce453b
Alan Krajina <alankrajina88@gmail.com>
2019-07-24 15:04:55 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,8 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    @user = User.find(session[:user_id])
+    erb :'/tweets/tweets'
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,25 +23,31 @@ class UsersController < ApplicationController
 
 
 
-  get '/account' do
-    @user = User.find(session[:user_id])
-    redirect '/tweets'
-  end
+#  get '/account' do
+#    @user = User.find(session[:user_id])
+#    redirect '/tweets'
+#  end
 
 
   get "/login" do
-    erb :'/users/login'
+    if logged_in?
+      redirect '/tweets'
+    else
+        erb :index
+    end
   end
 
   post "/login" do
-    user = User.find_by(:username => params[:username])
+    user1 = User.find_by(:username => params[:username])
 #binding.pry
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/account"
+    if user1 && user1.authenticate(params[:password])
+      session[:user_id] = user1.id
+    #  redirect "/account"
 #    else
 #      redirect "/failure"
     end
+    @user = User.find(session[:user_id])
+    redirect '/tweets'
   end
 
 
