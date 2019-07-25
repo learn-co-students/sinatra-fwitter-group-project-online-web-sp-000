eb1dd48e6ac52b7bf30ce7252f272ee51fa896d1
Alan Krajina <alankrajina88@gmail.com>
2019-07-25 10:21:40 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
 
-<<<<<<< HEAD
   get '/signup' do
       if !logged_in?
           erb :'/users/create_user'
@@ -16,43 +15,37 @@ class UsersController < ApplicationController
         @user = User.create(params)
         session[:user_id] = @user.id
         redirect "/tweets"
-=======
-  get "/signup" do
-    erb :'/users/create_user'
   end
+end
+
 
   post "/signup" do
 	@user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
 
 	  if @user.save && @user[:username] != ""
 	    erb :"/tweets/tweets"
->>>>>>> e5ccc3bc27a27f292f6cdff83150df1b7d5595a6
     end
   end
 
 
 
 
-<<<<<<< HEAD
   get "/login" do
     if logged_in?
       redirect '/tweets'
     else
-        erb :index
+      erb :'/users/login'
     end
   end
 
   post "/login" do
     user1 = User.find_by(:username => params[:username])
-#binding.pry
     if user1 && user1.authenticate(params[:password])
       session[:user_id] = user1.id
-    #  redirect "/account"
-#    else
-#      redirect "/failure"
+      redirect "/tweets"
+    else
+    redirect "/signup"
     end
-    @user = User.find(session[:user_id])
-    redirect '/tweets'
   end
 
 
@@ -65,29 +58,13 @@ class UsersController < ApplicationController
       redirect "/"
     end
   end
-  #    elsif !logged_in? &&
-  #      redirect "/"
-  #    elsif !logged_in?
-  #      redirect '/login'
-=======
 
 
-  get "/login" do
-    erb :'/users/login'
-  end
-
-  post "/login" do
-#    user = User.find_by(:username => params[:username])
-
-#    if user && user.authenticate(params[:password])
-#      session[:user_id] = user.id
-#      redirect "/account"
-#    else
-#      redirect "/failure"
-#    end
-  end
 
 
->>>>>>> e5ccc3bc27a27f292f6cdff83150df1b7d5595a6
+
+
+
+
 
 end
