6acfd0d4250e9f18ebdf147533f896ab9bf6bb74
Ramon Chavez <rachavez1974@gmail.com>
2019-07-30 17:36:22 -0400

    Add slug to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates_presence_of :password
+
+  def slug
+    self.username.gsub(' ', '-').downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.where("lower(username) =?", slug.split('-').join(' ')).first
+  end
 end
