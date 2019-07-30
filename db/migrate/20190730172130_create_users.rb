d69f35879db833c4c102a9ee9489e77701e0237a
Ramon Chavez <rachavez1974@gmail.com>
2019-07-30 14:10:03 -0400

    Change user_name to username in migration

--- db/migrate/20190730172130_create_users.rb
+++ db/migrate/20190730172130_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :password_digest
-      t.string :user_name, index: true
+      t.string :username, index: true
     end
   end
  
