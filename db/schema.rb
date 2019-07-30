cdccd684f2921d4576dc173ae4e5054e85d3fa76
Ramon Chavez <rachavez1974@gmail.com>
2019-07-30 13:36:39 -0400

    Add migration for tweets table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190730172130) do
+ActiveRecord::Schema.define(version: 20190730173154) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string  "title"
+    t.text    "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
