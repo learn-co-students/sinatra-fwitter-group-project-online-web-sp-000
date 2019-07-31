9321d3b899270be308f5a95f8ea25bd1ea6d8107
Ramon Chavez <rachavez1974@gmail.com>
2019-07-30 22:37:34 -0400

    Add sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    use Rack::Flash
     set :session_secret, "{SecureRandom.urlsafe_base64}"
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
   get '/signup' do
     erb :'users/new'
   end  
+
+  post '/signup' do
+    @user = User.new(params)
+    if @user.save 
+      flash[:message] = "Welcome to Flat Iron Twiter!!!"
+      redirect to "/tweets"
+    else
+      redirect to '/signup'
+    end
+  end
 end
