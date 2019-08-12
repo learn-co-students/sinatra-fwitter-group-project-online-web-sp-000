89d84046c0eead1a6e7a05fee311a6578828babb
Shawn <shawntennity@gmail.com>
2019-08-12 13:06:14 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,69 @@
+require 'rack-flash'
+
 class UsersController < ApplicationController
+  use Rack::Flash
+
+  get '/signup' do
+    # binding.pry
+    if is_logged_in?(session)
+      flash[:message] = "You are already logged in."
+      redirect '/tweets'
+    end
+      erb :"/users/signup"
+  end
+
+  post '/signup' do
+    # binding.pry
+    @user = User.create(params)
+    if @user.id == nil 
+      flash[:message] = "Invalid user credentials.  Please try again."
+      redirect '/signup'
+    else
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    end
+  end
+
+  get '/login' do
+    if is_logged_in?(session)
+      flash[:message] = "You are already logged in."
+      redirect '/tweets'
+    end
+      erb :"/users/login"
+  end
+  
+  post '/login' do
+    # binding.pry
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+
+      redirect "/tweets"
+    else
+      flash[:message] = "Incorrect login credentials.  Please try again."
+      redirect "/login"
+    end
+  end
+
+ 
+
+  get '/logout' do
+    if is_logged_in?(session)
+      session.clear
+      redirect "/login"
+    else
+      redirect "/"
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    @tweets = @user.tweets
+    erb :"/users/show"
+  end 
+
+
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  validates_presence_of :username, :email, :password
   has_secure_password
   has_many :tweets
+
+  def self.find_by_slug(slug)
+    result = self.all.select {|elem| elem.slug == slug }.first      
+  end
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
 end
