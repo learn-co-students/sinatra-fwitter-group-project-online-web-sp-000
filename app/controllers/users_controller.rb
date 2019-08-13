f2592e1915f693b8d0f1d053d18d645b70576344
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-13 21:09:38 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,54 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+  get '/signup' do # Loads form to sign up
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do # Receives submitted form
+    if valid_new_user?
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    else
+      redirect "/signup"
+    end
+  end
+
+  get '/login' do # Loads form to log in
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do # Receives submitted form to log in
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/logout' do # Clear the session hash, and redirect the user to /login.
+    if logged_in?
+      session.destroy
+      redirect "/login"
+    else
+      redirect "/"
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def slug
+    self.username.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|obj| obj.slug == slug}
+  end
+
 end
