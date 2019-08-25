1741cb9693e47ffa0e36f2e7d147c9bd1fd06f34
Yingqi Chen <chanwk18@hotmail.com>
2019-08-25 05:42:43 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,64 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    else
+      erb :"/users/create_user"
+    end
+  end
+
+  post '/signup'do
+    #binding.pry
+    if Helpers.empty_input?(params)
+      redirect to '/signup'
+    else
+      @user=User.create(params)
+      session[:user_id]=@user.id
+      redirect to '/tweets'
+    end
+  end
+
+  get '/login' do
+    #binding.pry
+    if Helpers.is_logged_in?(session)
+      redirect '/tweets'
+    else
+      erb :"/users/login"
+    end
+  end
+
+  post '/login' do
+    #binding.pry
+    @user=User.find_by(:username=>params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id]=@user.id
+      redirect to '/tweets'
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/logout' do
+    if Helpers.is_logged_in?(session)
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
+
+
+
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,66 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if Helpers.is_logged_in?(session)
+      @tweets=Tweet.all
+      @user=User.find(session[:user_id])
+      erb :"/tweets/index"
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if Helpers.is_logged_in?(session)
+      erb :'/tweets/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    #binding.pry
+    unless params[:content].empty?
+      @tweet=Tweet.create(:content=>params[:content])
+      @user=User.find(session[:user_id])
+      @user.tweets<<@tweet
+      redirect to '/tweets'
+    else
+      redirect to '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id' do
+    if Helpers.is_logged_in?(session)
+      @tweet=Tweet.find(params[:id])
+      erb :'/tweets/show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    #binding.pry
+    @tweet=Tweet.find(params[:id])
+    if Helpers.current_user(session)==@tweet.user
+      erb :'/tweets/edit'
+    elsif Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    unless params[:content].empty?
+      @tweet=Tweet.find(params[:id])
+      @tweet.update(:content=>params[:content])
+      redirect to '/tweets'
+    else
+      redirect to '/tweets/<%= @tweet.id %>/edit'
+    end
+  end
+
+
 
 end
