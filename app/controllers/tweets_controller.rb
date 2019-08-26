dd2cd73136fa2f488a0743ecafc1b1914d62527b
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-26 17:05:25 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,39 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    erb :'/tweets/index'
+  end
 
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
+
+  post '/tweets' do
+    if !logged_in?
+      redirect '/'
+    end
+    if params[:content] != ""
+      @tweet = Tweet.create(content: params[:content], user_id: current_user.id)
+      redirect "/tweets/#{@tweet.id}"
+    else
+      redirect '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    erb :"/tweets/#{@tweet.id}"
+  end
+
+  patch '/tweets/:id' do
+
+  end
+
+  post '/tweets/:id/delete' do
+
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,39 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'/users/signup'
+  end
 
+  post '/signup' do
+    if params[:username] != "" && params[:email] != "" && params[:password] != ""
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect "/tweets/#{@user.id}"
+    else
+      redirect '/users/signup'
+    end
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do # creating a session, adding key/value pair to session hash
+    @user = User.find_by(username: params[:username])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/tweets/index"
+    else
+      redirect '/users/login'
+    end
+  end
+
+  get '/users/:id' do
+    @user = User.find_by(id: params[:id])
+    erb :'/users/show'
+  end
+
+  get '/logout' do
+    redirect '/users/login'
+  end
 end
