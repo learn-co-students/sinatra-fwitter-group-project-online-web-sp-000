4f997d4e8f2d2c0e10ae70fab28357807196a240
Malcolm Leake <echelon1089@gmail.com>
2019-08-29 23:54:05 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    # @user = User.create(username: params[:username], :email params[:email], password: params[:password])
-    # redirect erb :"/tweets"
+     @user = User.create(username: params[:username], :email params[:email], password: params[:password])
+     redirect erb :"/tweets"
   end
 end
