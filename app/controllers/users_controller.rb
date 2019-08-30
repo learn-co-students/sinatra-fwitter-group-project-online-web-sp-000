ce3acfad56312b14062b0fbdfe020607b3d8dee2
Malcolm Leake <echelon1089@gmail.com>
2019-08-30 19:47:21 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,15 @@ class UsersController < ApplicationController
      @user = User.create(username: params["username"], email: params["email"], password: params["password"])
      session[:user_id] = @user.id
      redirect "/tweets"
-
-
-
   end
+
+  get '/login' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    end
+  end
+
+
+
+
 end
