b224a272418c9f4a5cdd1338af81cfa4a647b3af
Malcolm Leake <echelon1089@gmail.com>
2019-08-30 20:51:40 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,15 +11,31 @@ class UsersController < ApplicationController
         redirect '/signup'
       end
     end
-     @user = User.create(username: params["username"], email: params["email"], password: params["password"])
-     session[:user_id] = @user.id
+     user = User.create(username: params["username"], email: params["email"], password: params["password"])
+     session[:user_id] = user.id
      redirect "/tweets"
+
+     if Helpers.is_logged_in?
+
+     end
   end
 
   get '/login' do
     if Helpers.is_logged_in?(session)
       redirect to '/tweets'
     end
+    erb :"users/login"
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params["username"])
+     if user && user.authenticate(params[:password])
+       session[:user_id] = user.id
+       redirect to '/tweets'
+     else
+       flash[:login_error] = "Incorrect username and/or password. Please try again"
+       redirect to 'login'
+     end
   end
 
 
