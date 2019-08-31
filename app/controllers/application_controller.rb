887d963120113b55b082769931f1037e7988ae07
Meisuleen Kased <meisuleen@gmail.com>
2019-08-31 02:46:43 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,5 +14,28 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  #helper methods
+  helpers do
+    def current_user
+      # Finds the current user, whose id is the same as the session's user_id. Or return nil.
+      # Note that #find breaks with an ActiveRecord::RecordNotFound error if it can't find a user, but #find_by_id returns nil.
+
+      # The following code either returns (an already defined) @user or queries the database and stores the result in @user.
+      # This prevents multiple unnecessary database queries that would slow down the program.
+
+      # However, when you logout a user in one of the routes, current_user won't be nil until you exit that route.
+      # If you want current_user to be nil (while still in the logout route), then omit "@user ||=".
+
+      @user ||= User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+      # Returns a boolean value: true if the user is logged in, false if not.
+      # The user is logged in if current_user is truthy (because the session's user_id is not nil).
+
+      !!current_user
+    end
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
 class UsersController < ApplicationController
 
+  get '/' do
+   erb :index
+  end
+
+  get '/signup' do
+   # if the user is already signed up, redirect to Tweets
+   # if the is not signed up- sign up, direct to create user form
+   erb :'users/signup'
+  end
+
+  post '/signup' do
+   # creates a new user
+
+
+  end
+
+  get '/login' do
+   # if a user is already logged in, they will be redirected to Tweets page
+   # if the user is not logged in, redirect them to the login page (maybe use an error message)
+
+  end
+
+  post '/login' do
+   # create session for logged in user
+   # if logged in redirect to Tweets
+   # if not logged in redirect to login page
+
+  end
+
+  get '/logout' do
+   session.clear
+
+   redirect '/login'
+  end
 
 end
