9dfef136533c7a80f699fb8b3f09d854bc109fa5
Malcolm Leake <echelon1089@gmail.com>
2019-09-05 21:15:38 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190829204153) do
+ActiveRecord::Schema.define(version: 20190905211003) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string "content"
+    t.string  "content"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
