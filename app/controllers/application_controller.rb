65de8363294c0fa1e02065ba41debb1d1551973b
Becky Phan <beckyphan@live.com>
2019-09-10 22:07:13 -0400

    moved login/logout to users_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,53 +13,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    if logged_in?
-      redirect '/tweets'
-    else
-      erb :signup
-    end
-  end
-
-  post '/signup' do
-    @user = User.new(params[:user])
-
-    if @user.save
-      session[:user_id] = @user.id
-      redirect "/tweets"
-    else
-      redirect '/signup'
-    end
-  end
-
-  get '/login' do
-    if logged_in?
-      redirect '/tweets'
-    else
-      erb :'users/login'
-    end
-  end
-
-  post '/login' do
-    @user = User.find_by(username: params[:username])
-
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect '/tweets'
-    else
-      redirect "/login"
-    end
-  end
-
-  get '/logout' do
-    if logged_in?
-      session.clear
-      redirect "/login"
-    else
-      redirect '/'
-    end
-  end
-
   helpers do
     def current_user
       @current_user ||= User.find_by(id: session[:user_id])
