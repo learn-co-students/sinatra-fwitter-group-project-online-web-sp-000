f04c12bff6c499f71b4dece20880e614bcc1042f
Peter Grant <jgrantps@hotmail.com>
2019-09-12 14:40:49 +0000

    down to 14 errors

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,23 +10,39 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/new' do
-    erb :'tweets/new'
+    if logged_in?
+      erb :'tweets/new'
+    else
+      redirect '/login'
+    end
   end
 
   post '/tweets' do
-    # binding.pry
     @tweet = Tweet.create(content: params[:content], user_id: session[:user_id])
     current_user.tweets << @tweet
     redirect :"tweets/#{@tweet.id}"
   end
 
   get '/tweets/:id' do
-    @tweet = Tweet.find_by(id: params[:id])
-    erb :'tweets/show_tweet'
+    if logged_in?
+      @tweet = Tweet.find_by(id: params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect to :"/login"
+    end
   end
 
   get '/tweets/:id/edit' do
-    erb :'tweets/edit_tweet'
+    if logged_in?
+    @tweet = Tweet.find_by(id: params[:id])
+      if @tweet.belongs_to current_user
+        erb :'tweets/edit_tweet'
+      else
+        redirect to :"/tweets"
+      end
+    else
+      redirect to :"/login"
+    end
   end
 
 end
