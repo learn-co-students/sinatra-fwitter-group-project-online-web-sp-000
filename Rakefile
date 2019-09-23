e05641999e91cf50b04cdd1387468171b56d4a30
rehman ali <rehmanali001@gmail.com>
2019-09-23 12:54:10 -0500

    changes

--- Rakefile
+++ Rakefile
@@ -6,3 +6,9 @@ require 'sinatra/activerecord/rake'
 
 
 # Type `rake -T` on your command line to see the available rake tasks.
+
+task :remigrate do
+  Rake::Task["db:rollback"].execute
+  Rake::Task["db:rollback"].execute
+  Rake::Task["db:migrate"].execute 
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,24 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions #unless test?
+    set :session_secret, "password_security"
   end
 
+  get '/' do
+
+    erb :"index"
+  end
+
+  helpers do
+    def current_user(session) 
+      # binding.pry
+      @user = User.find_by_id(session[:user_id])
+    end
+  
+    def is_logged_in?(session)
+      # binding.pry
+      !!session[:user_id]
+    end
+  end
 end
