c320e4b506e889e149e9389c9d4169cd8c320b08
Tracy Reuther <tracyreuther@gmail.com>
2019-09-24 21:59:14 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,41 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    if params[:username] != "" && params[:email] != "" && params[:password] != ""
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect :'/signup'
+    else
+      # @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       @user = User.create(params)
       session[:user_id] = @user_id
+
       redirect :'/tweets'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
     else
-      redirect :'/signup'
+      redirect :'/tweets'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect :'/tweets'
+      else
+        redirect :'/login'
+      end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect :'/login'
+    else
+      redirect :'/'
     end
   end
 
