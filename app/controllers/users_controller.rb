594ff503bdd6f01455ec06e80ec89d8037a1a877
Tracy Reuther <tracyreuther@gmail.com>
2019-09-24 00:04:32 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,11 @@ class UsersController < ApplicationController
     erb :'/users/create_user'
   end
 
+  post '/signup' do
+    @user = User.new(uername: params[:username], email: params[:email], password: params[:password])
+
+    if params[:username] && params[:password]
+    redirect :'/tweets'
+  end
+
 end
