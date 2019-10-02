e48369d3a8e89346790c9264f4809c35b459adc7
Ranika Williams <rnkprice07@gmail.com>
2019-10-02 16:09:42 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,66 @@
 class UsersController < ApplicationController
-
-    get '/users' do
-        @users = User.all
-        erb :'users/index'
+   use Rack::Flash
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+    
+  get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
     end
 
+    erb :"/users/create_user"
+  end
 
+  post '/signup' do
+    params.each do |label, input|
+      if input.empty?
+        flash[:new_user_error] = "Please enter a value for #{label}"
+        redirect to '/signup'
+      end
+    end
+
+    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+    session[:user_id] = user.id
+
+    redirect to '/tweets'
+  end
+
+  get '/login' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    else
+      erb :"users/login"
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params["username"])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      flash[:login_error] = "Incorrect login. Please try again."
+      redirect to '/login'
+    end
+  end
+  
+  get '/logout' do
+    session.clear
+    redirect to '/login'
+
+    if Helper.is_logged_in?(session)
+      erb :'/tweets/tweets'
+    end
+  end
+  
+  get '/users/:slug' do
+    slug = params[:slug]
+    @user = User.find_by_slug(slug)
+    erb :"users/show"
+  end
 end
