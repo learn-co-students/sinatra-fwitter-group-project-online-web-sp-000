c8a2e98fc878f4abf4e9fe50b50fb270cec1adf0
Alice Brunel <alc.brunel@gmail.com>
2019-10-04 12:32:09 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :'users/show'
+    end
+
   get '/signup' do
       if !logged_in?
         erb :'/users/signup', locals: {message: "Please sign up before you sign in"}
@@ -25,5 +30,25 @@ class UsersController < ApplicationController
       redirect to '/tweets'
     end
   end
+
+  post '/login' do
+    @user = User.find_by( username: params[:username] )
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def slug
+    slug_name = self.username.downcase.split(" ")
+    slug_name.join("-")
+  end
+
+  def self.find_by_slug(slug_name)
+    User.all.find{|user| user.slug == slug_name}
+  end
+
 end
