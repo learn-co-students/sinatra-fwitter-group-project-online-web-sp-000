483025ff8f495a30188d27be349c0874bbabc924
Alice Brunel <alc.brunel@gmail.com>
2019-10-04 11:25:42 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,12 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-
-    erb :'/tweets'
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'/tweets/index'
+    else
+      redirect '/login'
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,29 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    erb :'/users/signup'
+      if !logged_in?
+        erb :'/users/signup', locals: {message: "Please sign up before you sign in"}
+      else
+        redirect to '/tweets'
+      end
+  end
 
+  post '/signup' do
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
+      redirect to '/signup'
+    else
+      @user = User.create( username: params[:username], email: params[:email], password: params[:password] )
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/tweets'
+    end
   end
 
 end
