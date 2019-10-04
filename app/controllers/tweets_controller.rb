b55f9f369805c6d0c750d4ca5062fba8596785b3
Alice Brunel <alc.brunel@gmail.com>
2019-10-04 13:04:02 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,4 +9,29 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      redirect :'/login'
+    end
+  end
+
+  post '/tweets' do
+    if logged_in?
+      if params[:content] == ""
+        redirect '/tweets/new'
+      else
+        @tweet = Tweet.create( content: params[:content] )
+        @tweet.user_id = current_user.id
+        if @tweet.save
+          redirect "/tweets/#{@tweet.id}"
+        else
+          redirect '/tweets/new'
+        end
+      end
+    else
+      redirect '/login'
+    end
+  end
 end
