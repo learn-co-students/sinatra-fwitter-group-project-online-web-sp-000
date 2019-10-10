ceda77e62f0d07a38f854b5541e2c8a9badb5a72
Tom White <eugene.white@gmail.com>
2019-10-10 21:25:18 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'/users/signup'
+  end
+
+  post '/signup' do
+    #user = User.new(:username => params[:username], :password => params[:password])
+
+		#if user.username != "" && user.save
+		  #redirect "/login"
+		#else
+		  redirect to '/tweets'
+		#end
+  end
 
 end
