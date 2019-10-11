7a54c7415a5d6fa2ebc5ca5f34a726ad2a314baf
Eric Clark <eclark0013@gmail.com>
2019-10-11 16:52:32 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,60 @@
 class TweetsController < ApplicationController
+    
+    get '/tweets/new' do
+        if logged_in?
+            erb :'/tweets/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/tweets' do
+        if params[:content] == ""
+            redirect '/tweets/new'
+        else
+            User.find(session[:user_id]).tweets.build(content: params[:content]).save
+        end
+    end
+
+    get '/tweets/:id' do
+        if logged_in?
+            @tweet = Tweet.find(params[:id])
+            erb :'/tweets/show_tweet'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        if logged_in?
+            @tweet = Tweet.find(params[:id])
+            if session[:user_id] == @tweet.user_id
+                erb :'/tweets/edit_tweet'
+            end
+        else
+            redirect '/login'  
+        end
+    end
+
+    patch '/tweets/:id/edit' do
+        if params[:content] != ""
+            @tweet = Tweet.find(params[:id])
+            @tweet.content = params[:content]
+            @tweet.save
+        end
+        redirect "/tweets/#{@tweet.id}"
+    end
+
+    delete '/tweets/:id/delete' do
+        if logged_in?
+            @tweet = Tweet.find(params[:id])
+            if session[:user_id] == @tweet.user_id
+                @tweet.destroy
+            end
+        else
+            redirect '/login'  
+        end
+    end
 
 
 end
