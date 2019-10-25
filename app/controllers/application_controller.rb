3c3456fa3a1eb3c26ba010c1b3c988aada17b818
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-10-24 21:16:43 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "some_secrete_code"
+  end
+
+  get '/' do
+    erb :index
   end
 
 end
