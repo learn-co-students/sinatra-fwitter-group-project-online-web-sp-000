4a80f84f21cbbaec68ace9af0502f4023569cb52
Lauren McCoy <lm_lutec@outlook.com>
2019-10-27 06:58:15 -0400

    no updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 require './config/environment'
-
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions 
+    set :session_secret, 'secret'
+  end
+
+  helpers do
+
   end
 
   get '/' do 
@@ -12,7 +18,29 @@ class ApplicationController < Sinatra::Base
   end 
 
   get '/signup' do 
-    erb:'/signup'
+      erb:'/signup'
   end 
+
+  post '/signup' do 
+    if params[:username] != "" && params[:email] != "" && params[:password] != "" 
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id 
+      redirect to '/tweets'
+      else  
+      redirect to '/signup'
+    end 
+  end 
+
+  get '/login' do  
+    erb:'/login'
+  end 
+
+  get '/tweets' do 
+    if session[:user_id]
+
+    end 
+  end
+
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
 
 
-    get '/users/new' do 
-        erb:'/users/new'
-    end 
+   
 
 end
