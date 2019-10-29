8fe37e4e0caa8e771bfe15b4cc7b635b5f6f510c
Lauren McCoy <lm_lutec@outlook.com>
2019-10-29 14:21:28 -0400

    tweets/new

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -15,9 +15,14 @@ class TweetsController < ApplicationController
     end 
 
     post '/tweets' do 
-        @tweet = Tweet.create(content: params[:content])
-        current_user.tweets << @tweet
+        if params[:content] != ""
+            @tweet = Tweet.create(content: params[:content])
+            current_user.tweets << @tweet
+        else 
+            redirect to "/tweets/new"
+        end 
     end 
 
+    
 
 end
