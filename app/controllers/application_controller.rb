65af2d83a6694e3db4025e6261fbe95de9b1f013
Austin Asbury <acasbury25@aol.com>
2019-12-15 04:10:18 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
-
+    @user = User.new(username: params["username"], email: params["email"], password: params["password"])
+    @user.save
+    session[:user_id] = @user.id
+    redirect '/tweets'
   end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
 
-
+  get '/tweets' do
+    erb :'/tweets/tweets'
+  end
 end
