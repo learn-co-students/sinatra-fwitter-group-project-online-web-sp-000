f4d0b1ba90f168ef4cb4b0307644c57d8b4c32e4
Austin Asbury <acasbury25@aol.com>
2019-12-17 20:26:22 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,16 @@ class TweetsController < ApplicationController
     if session[:user_id] == nil
       redirect '/login'
     else
+      @tweets = Tweet.all
       erb :'/tweets/tweets'
+    end
+  end
+
+  get '/tweets/new' do
+    if session[:user_id] == nil
+      redirect '/login'
+    else
+      erb :'/tweets/new'
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
-
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/page'
+  end
 
 end
