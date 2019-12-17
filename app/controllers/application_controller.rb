29ec363ad7ae92dd34acc8a63357f590d86c8086
Austin Asbury <acasbury25@aol.com>
2019-12-17 00:54:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,21 @@ class ApplicationController < Sinatra::Base
     end
   end
 
-  get 'login' do
-    erb :login
+  get '/login' do
+    if session[:user_id] != nil
+      redirect '/tweets'
+    else
+      erb :login
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    else
+      redirect "/login"
+    end
   end
 end
