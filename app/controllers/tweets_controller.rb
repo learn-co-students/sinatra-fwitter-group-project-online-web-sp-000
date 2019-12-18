128b789dd1f9e8cbf2daafa782af9cee0db5ee72
Austin Asbury <acasbury25@aol.com>
2019-12-18 18:00:26 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,4 +16,38 @@ class TweetsController < ApplicationController
       erb :'/tweets/new'
     end
   end
+
+  post '/tweets' do
+    if params[:content] == ""
+      redirect '/tweets/new'
+    else
+      Tweet.create(content: params[:content], user_id: session[:user_id])
+      redirect '/tweets'
+    end
+  end
+
+  get '/tweets/:id' do
+    if session[:user_id] == nil
+      redirect '/login'
+    else
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if session[:user_id] == nil
+      redirect '/login'
+    else
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/edit'
+    end
+  end
+
+  patch '/tweets/:id' do #edit action
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.content = params[:content]
+    @tweet.save
+    redirect "/tweets/#{@tweet.id}"
+  end
 end
