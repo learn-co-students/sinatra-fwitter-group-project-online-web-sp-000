b55a70a8240583a94f23d73337f2b329f9121a70
Nicholas Ng <nicholasykng@gmail.com>
2020-01-12 23:04:55 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,23 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "very_secret_information"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
