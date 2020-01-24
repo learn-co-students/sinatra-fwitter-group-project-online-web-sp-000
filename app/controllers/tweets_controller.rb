666bbbf2a13ec0f0eacbb03e730d93111ed90cc9
RickLearnsToCode <rick.rosetti@gmail.com>
2020-01-23 22:17:10 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,8 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        erb :'tweets/tweets'
+    end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,43 @@ class UsersController < ApplicationController
     end
 
     get '/signup' do
-        erb :signup
+        # binding.pry
+        if !logged_in?
+            erb :'users/signup'
+        else
+            redirect to '/tweets'
+        end
     end
 
     post '/signup' do
-        if params[:name] == "" || params[:email] == "" || params[:password] == ""
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
             redirect to '/signup'
         end
 
         @user = User.new(params)
         @user.save
-        # binding.pry
         session[:user_id] = @user.id
-        redirect to 'tweets/tweets'
+        # binding.pry
+        redirect to '/tweets'
     end
+
+    get '/login' do
+        erb :'users/login'
+
+    end
+
+
+    post '/login' do
+
+
+    end
+
+
+    get '/logout' do
+        session.clear
+        erb :users/login
+    end
+
 
 
 
