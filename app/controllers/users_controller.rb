f237e8fd261c0ed5aeec5accd4fbffb0e9cc5249
RickLearnsToCode <rick.rosetti@gmail.com>
2020-01-23 19:58:18 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
 
+    get '/' do
+      erb :index
+    end
+
+    get '/signup' do
+        erb :signup
+    end
+
+    post '/signup' do
+        if params[:name] == "" || params[:email] == "" || params[:password] == ""
+            redirect to '/signup'
+        end
+
+        @user = User.new(params)
+        @user.save
+        # binding.pry
+        session[:user_id] = @user.id
+        redirect to 'tweets/tweets'
+    end
+
+
+
 
 end
