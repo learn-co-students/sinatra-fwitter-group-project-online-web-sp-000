61dd6d181cf860efebfac4e796f40ae8b25fa889
github username <github email address>
2020-02-03 13:21:30 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,43 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+          #binding.pry
+    if !!session[:user_id]
+      redirect to "/tweets"
+    else
+      erb '/users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if !params[:username].empty? && !params[:password].empty? && !params[:email].empty?
+      @user = User.new(:username => params[:username], :password => params[:password], :email => params[:email])
+      if @user.save
+        redirect "/tweets"
+      end
+    else
+      redirect "/signup"
+    end
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post "/login" do
+    @user = User.find_by(:username => params[:username])
+    if !params[:username].empty? && !params[:password].empty && !params[:email].empty && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/tweets"
+    else
+      redirect "/login"
+    end
+  end
+
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
