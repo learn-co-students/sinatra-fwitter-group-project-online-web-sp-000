524394714f00959ecf2a54bf02aa6d7169637cfe
Chris Cheong <chrisxcheong@gmail.com>
2020-02-08 08:15:45 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,13 @@
 class TweetsController < ApplicationController
+  get '/' do
+    if current_user
+      erb :"/home"
+    else
+      redirect to '/login'
+    end
+  end
 
-
+    get '/login' do
+      erb :'/login'
+    end
 end
