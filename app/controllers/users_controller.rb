4f63508a277c5aac9a87c2c2281145caec0faae7
Chris Cheong <chrisxcheong@gmail.com>
2020-02-09 10:58:14 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,39 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    @user = User.create(params)
-    session[:user_id] = @user.id
-    redirect to '/tweets'
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    else
+      redirect to '/signup'
+    end
   end
 
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   # validates :email, uniqueness: true
-  # validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  #
   # validates :username, uniqueness: true
   # Validates :password, presence: true
+  validates :email,:username,:password, :presence => true
+  validates :email,:username, :uniqueness => true
+  validates :email,:username, :uniqueness => { case_sensitive: false }
 
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
   has_secure_password
   has_many :tweets
