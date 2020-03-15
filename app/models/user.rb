69082bff4460531dc0aa73dd053e2bac7a2123e5
imjoshellis <joshe523@gmail.com>
2020-03-15 18:32:15 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def slug
+    username.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    all.each do |o|
+      return o if o.slug == slug
+    end
+  end
 end
