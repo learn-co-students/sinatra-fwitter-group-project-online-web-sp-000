2e41e4ae71fd5c272f7c6eeeff8bf0c1098a7246
lukesherwood <luke.sherwood55@gmail.com>
2020-04-04 13:40:11 +1300

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,23 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views, proc { File.join(root, '../views/') }
+    enable :sessions unless test?
+    set :session_secret, "secret"
   end
+
+  get '/' do
+    erb :'/index'
+  end
+
+  helpers do
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
+	end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,75 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        if logged_in? 
+            erb :'/tweets/tweets' 
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/tweets/new' do
+        if logged_in? 
+            erb :'/tweets/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/tweets' do
+        #can this be refeactored?
+        if !params[:content].empty?
+            tweet = Tweet.new(params)
+            tweet.user_id = current_user.id
+            tweet.save
+            redirect "/tweets/#{tweet.id}"
+        else
+            redirect '/tweets/new'
+        end
+    end
+
+    get '/tweets/:id/delete' do
+        @tweet = Tweet.find(params[:id])
+        if logged_in? && @tweet.user_id == current_user.id
+            @tweet.destroy
+            redirect "/tweets"
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        if logged_in? 
+            @tweet = Tweet.find(params[:id])
+            erb :'/tweets/edit_tweet'
+        else
+            redirect '/login'
+        end
+    end
+
+    patch '/tweets/:id' do
+        @tweet = Tweet.find_by(params[:id])
+        if !params[:content].empty?
+            @tweet.update(content: params[:content])
+            @tweet.save
+            redirect "/tweets/#{@tweet.id}"
+        else
+            redirect "/tweets/#{@tweet.id}/edit"
+        end 
+    end
+
+    get '/tweets/:id' do
+        if logged_in? 
+            @tweet = Tweet.find(params[:id])
+            erb :'/tweets/show_tweet'
+        else
+            redirect '/login'
+        end
+
+    end
+
+    
+
+
 
 end
