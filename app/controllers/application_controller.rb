4f8a68dcb4bb0bf8d8f06085842e92d88de76434
Kirsten Keels <keelsk@gmail.com>
2020-04-05 00:08:34 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,25 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "fwitter"
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    @session = session
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      @user = User.find(session[:user_id])
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,45 @@
+require 'pry'
 class UsersController < ApplicationController
 
+  get '/signup' do
+    redirect "/tweets" if logged_in?
+    erb :'users/signup'
+  end
+
+  post '/signup' do
+    redirect "/signup" if params[:username] == "" || params[:email] =="" || params[:password] == ""
+    user = User.create(params)
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    redirect '/tweets' if logged_in?
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'tweets/show'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
 
 end
