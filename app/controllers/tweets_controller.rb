f9f0ccc260805ec0ae2ed4a644675873c4564a4f
LIManuel <leonmalisov@gmail.com>
2020-04-10 15:45:46 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,72 @@
 class TweetsController < ApplicationController
 
+	get "/tweets" do
+		if session[:user_id]
+			@user = User.find(session[:user_id])
+			@users = User.all
+			erb :"tweets/index"
+		else
+			redirect "/login"
+		end
+	end
 
+	get "/tweets/new" do
+		if session[:user_id]
+			@user = User.find(session[:user_id])
+			erb :"tweets/new"
+		else
+			redirect "/login"
+		end
+	end
+
+	post "/tweets" do
+		if params[:content].empty?
+			redirect "/tweets/new"
+		end
+
+		user = Helpers.current_user(session)
+		tweet = Tweet.create(content: params[:content])
+		user.tweets << tweet
+
+		redirect "/tweets/#{tweet.id}"
+	end
+
+	get "/tweets/:id" do
+		if Helpers.is_logged_in?(session)
+			@tweet = Tweet.find(params[:id])
+			erb :"tweets/show"
+		else
+			redirect "/login"
+		end
+	end
+
+	get "/tweets/:id/edit" do
+		redirect "/login" if Helpers.is_logged_in?(session) == false		
+
+		@tweet = Tweet.find(params[:id])
+
+		if Helpers.is_logged_in?(session) && @tweet.user_id == Helpers.current_user(session).id
+			erb :"tweets/edit"
+		end
+	end
+
+	patch "/tweets/:id" do
+		tweet = Tweet.find(params[:id])
+
+		if params[:content].empty?
+			redirect "/tweets/#{tweet.id}/edit"
+		end
+
+		tweet.update(content: params[:content])
+		redirect "/tweets/#{tweet.id}"
+	end
+
+	delete "/tweets/:id/delete" do
+		tweet = Tweet.find(params[:id])
+		if tweet.user_id == Helpers.current_user(session).id
+			tweet.destroy
+			redirect "/tweets"
+		end
+		redirect "/login"
+	end
 end
---
+++ app/models/helpers.rb
@@ -0,0 +1,11 @@
+class Helpers
+	attr_accessor :id
+
+	def self.current_user(session_hash)
+		User.find_by(id: session_hash[:user_id])
+	end
+
+	def self.is_logged_in?(session_hash)
+		!!session_hash[:user_id]
+	end
+end
