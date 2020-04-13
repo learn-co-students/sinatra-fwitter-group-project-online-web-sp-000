d75e717e78b653da70eb84e3698c7de36597fb19
Michael Carroll <carrollm2@gmail.com>
2020-04-13 02:40:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+
+    enable :sessions
+    set :session_secret, "password_security"    
   end
 
 end
