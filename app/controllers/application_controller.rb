2ddf661df9eb59dbbea999996155034095d2792b
Michael Carroll <carrollm2@gmail.com>
2020-04-13 23:20:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,12 +15,41 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-
+    erb :'signup'
   end
 
   post '/signup' do
-    redirect 'tweets/index'
+    user = User.new(:username => params[:username], :password => params[:password])
+
+    if params[:username] == "" or params[:email] == "" or paams[:password] == ""
+      #redirect "/failure"
+      redirect "/signup"
+    else
+      user.save
+      #redirect "/login"
+      redirect 'tweets/index'
+	  end
+
   end
 
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+
+    user = User.find_by(:username => params[:username])
+
+	  if user && user.authenticate(params[:password])
+	    session[:user_id] = user.id
+	    redirect "/tweets/index"
+	  else
+	    redirect '/failure'
+	  end
+  end
+
+  get '/failure' do
+    erb :failure
+  end
 
 end
