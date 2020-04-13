3abe7558e7d88b4f697cf6013e74c5da7110ea66
Michael Carroll <carrollm2@gmail.com>
2020-04-13 03:27:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,11 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
 
     enable :sessions
-    set :session_secret, "password_security"    
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :'layout'
   end
 
 end
