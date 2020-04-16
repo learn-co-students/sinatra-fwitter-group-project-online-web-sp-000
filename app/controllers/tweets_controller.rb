bb938623c93c19d3b65ded846df8883d36965a08
Michael Carroll <carrollm2@gmail.com>
2020-04-16 02:33:57 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,5 +14,17 @@ class TweetsController < ApplicationController
     end
   end
 
+  post '/tweets' do
+
+    if Helpers.is_logged_in?(session)
+      if params["content"] == ""
+        redirect 'tweets/new'
+      else
+        @tweet = Tweet.create(content: params["content"], user_id: session[:user_id])
+        erb :'tweets/index'
+      end
+    end
+  end
+
 
 end
