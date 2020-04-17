10c78f7be460d1c1091c4012da2456ac0686dacf
Michael Carroll <carrollm2@gmail.com>
2020-04-17 01:49:04 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,5 +28,49 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/:id' do
+    if Helpers.is_logged_in?(session)
+      @tweet = Tweet.find_by(id: params[:id])
+      erb :'tweets/show'
+    else
+      redirect 'users/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if Helpers.is_logged_in?(session)
+      @tweet = Tweet.find_by(id: params[:id])
+      @user = User.find_by(id: @tweet.user_id)
+      if @user.id == session[:user_id]
+        erb :'tweets/edit'
+      end
+    else
+      redirect 'users/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+
+    @tweet = Tweets.find_by(id: params[:id])
+    @tweet.update(params["content"])
+
+    redirect "tweets/#{@tweet.id}"
+
+  end
+
+  delete '/tweets/:id' do
+    if Helpers.is_logged_in?(session)
+      @tweet = Tweet.find_by(id: params[:id])
+      @user = User.find_by(id: @tweet.user_id)
+      if @user.id == session[:user_id]
+        @tweet.delete
+        redirect "/tweets"
+      end
+    else
+      redirect 'users/login'
+    end
+
+  end
+
 
 end
