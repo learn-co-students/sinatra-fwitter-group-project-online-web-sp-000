00de05715b47f235a44f1a879f64f53545b72551
CoderJay06 <jaycru06@yahoo.com>
2020-04-17 23:59:37 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,7 +14,11 @@ class TweetsController < ApplicationController
 
    get '/tweets/new' do 
       @tweets = Tweet.all
-      erb :'./tweets/new'
+      if logged_in?
+         erb :'./tweets/new'
+      else 
+         redirect to "/login"
+      end 
    end
 
    post '/tweets' do 
