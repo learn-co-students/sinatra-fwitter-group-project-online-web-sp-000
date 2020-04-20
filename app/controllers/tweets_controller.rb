9a13aacc29a5cf9f9a75b4d6388bfb8c519136ab
Russell Macalintal <russell.macalintal@gmail.com>
2020-04-20 06:41:07 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,7 +14,23 @@ class TweetsController < ApplicationController
     if logged_in?
       erb :'/tweets/new'
     else
-
+      # erb :'/tweets/error'
+      redirect '/login'
     end
+  end
+
+  post '/tweets' do
+    if params[:content].empty?
+      redirect '/tweets/new'
+    else
+      @tweet = Tweet.create(params)
+      current_user.tweets << @tweet
+      redirect "/tweets/#{@tweet.id}"
+    end
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/show_tweet'
   end
 end
