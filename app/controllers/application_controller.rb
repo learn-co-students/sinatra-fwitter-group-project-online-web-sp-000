5deb99266279ca89c171b13206b9783a1ab3d881
github username <github email address>
2020-04-29 13:21:07 -0700

    Signup finished with uniqueness validation and no empty strings given for signup values

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,23 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
 
   get '/' do
     erb :index
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    new_user = User.create(params)
+    session[:id] = new_user.id
+    redirect '/tweets'
   end
 
 
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        if session[:id]
+            @tweets = Tweet.all
+            erb :'tweets/index'
+        else
+            redirect '/signup'
+        end
+    end
 
 end
