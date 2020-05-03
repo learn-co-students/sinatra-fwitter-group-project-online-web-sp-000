484263441a7c540add922ad5a7f97b395cba1cc0
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-03 18:36:02 -0400

    added tweets to DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_222510) do
+ActiveRecord::Schema.define(version: 2020_05_03_223020) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
