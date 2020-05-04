7a9445dd4a3e37d4e6c7ff1c3bd00dc6b0dca8af
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-04 12:45:03 -0400

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,25 @@ class UsersController < ApplicationController
     end
   end
 
-  get '/login' do
-    erb :'users/login'
-  end
-
   post '/signup' do
     @user = User.new(params[:user])
 
     if @user.save
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    else
+      redirect "/error"
+    end
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect "/tweets"
     else
