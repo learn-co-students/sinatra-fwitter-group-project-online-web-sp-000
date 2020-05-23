fd7037abefda0eb418e1838df8b3a53702891c40
zchesny <z.chesny@gmail.com>
2020-05-23 13:06:15 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,48 @@
 class UsersController < ApplicationController
+  get '/signup' do 
+    if logged_in?
+        redirect '/tweets'
+    else 
+        erb :'users/signup', locals: {message: "Please Sign Up before you Login"}
+    end
+  end
 
+  post '/signup' do 
+    if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+        user = User.create(params)
+        session[:user_id] = user.id
+	    redirect '/tweets'
+	else
+		redirect '/signup'
+	end
+  end
+
+  get '/login' do 
+    if !logged_in?
+        erb :'/users/login'
+    else 
+        redirect '/tweets'
+    end
+  end
+
+  post '/login' do 
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect '/tweets'
+    else 
+        redirect '/signup'
+    end 
+  end
+
+  get '/logout' do
+    if logged_in?
+        session.destroy
+        redirect '/login'
+    else 
+        redirect '/'
+    end
+  end
 
 end
