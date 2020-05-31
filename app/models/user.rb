450833a067717212a14079a5fbd70f437db67b32
Sergio Marelli <sergio.mrll@gmail.com>
2020-05-31 23:01:54 +0100

    error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  validates :email, :username, :password, presence: true
+
+  def slug
+  	self.username
+  end
+
+  def find_by_slug(slug)
+  	User.find_by(name: slug)
+  end
 end
