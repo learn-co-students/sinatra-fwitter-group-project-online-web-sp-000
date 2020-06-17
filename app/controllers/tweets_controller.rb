887e626f0ff426d3e7169b29e33f4481a0791a33
Ronda D Cowen <cowenhome5@gmail.com>
2020-06-17 20:01:40 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,39 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if !logged_in?
+      redirect to '/login'
+    end
+    @tweets = Tweet.all
+    erb :'/tweets/tweets'
+  end
+
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
+
+  post '/tweets' do
+    erb :'/tweets/new'
+  end
+
+  get '/tweets/:id' do
+    erb :'/tweets/show_tweet'
+  end
+
+  get '/tweets/:id/edit' do
+    erb :'/tweets/edit_tweet'
+  end
+
+  post '/tweets/:id' do
+
+  end
+  post '/tweets/:id/delete' do
+
+ end
 
 end
