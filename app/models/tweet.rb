68541a8960291955ca895a339218428fbd873166
Ronda D Cowen <cowenhome5@gmail.com>
2020-06-18 20:17:25 +0000

    Automatically backed up by Learn

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,15 +1,3 @@
 class Tweet < ActiveRecord::Base
-  belongs_to :user
-
-  # def slug
-  #     self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-  # end 
-
-  # def self.find_by_slug(slug)
-    
-  #   Song.all.detect do |result|
-  #     result.slug === @slug
-  #   end
-  # end
 
 end
