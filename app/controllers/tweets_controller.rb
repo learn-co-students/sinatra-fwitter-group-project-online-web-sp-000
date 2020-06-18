936c46d87cd94bf6259c2963e0993afdc7938374
Ronda D Cowen <cowenhome5@gmail.com>
2020-06-18 20:29:02 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,44 +1,55 @@
 class TweetsController < ApplicationController
-
-  get '/tweets' do
-    if logged_in?
-      @tweets = Tweet.all
-      erb :'tweets/tweets'
-    else
-      redirect to '/login'
+    get '/tweets' do
+      if logged_in?
+        @tweets = Tweet.all
+        erb :'tweets/tweets'
+      else
+        redirect to '/login'
+      end
     end
-  end
-
-  get '/tweets/new' do
-    if logged_in?
-      erb :'/tweets/new'
-    else
-      redirect to '/login'
+    get '/tweets/new' do
+      if logged_in?
+        erb :'/tweets/new'
+      else
+        redirect to '/login'
+      end
     end
-  end
-
-  post '/tweets' do
-    erb :'/tweets/new'
-  end
-
-  get '/tweets/:id' do
-   if logged_in?
-     @tweet = Tweet.find_by_id(params[:id])
-     erb :'tweets/show_tweet'
-   else
-     redirect to '/login'
+    post '/tweets' do
+      @tweet = Tweet.create(:content => params[:content])
+      redirect to "/tweets/#{@tweet.id}"
+    end
+    get '/tweets/:id' do
+     if logged_in?
+       @tweet = Tweet.find_by_id(params[:id])
+       erb :'tweets/show_tweet'
+     else
+       redirect to '/login'
+     end
    end
- end
-
-  get '/tweets/:id/edit' do
-    erb :'/tweets/edit_tweet'
-  end
-
-  post '/tweets/:id' do
-
-  end
-  post '/tweets/:id/delete' do
-
- end
-
-end
+    get '/tweets/:id/edit' do
+        @tweet = Tweet.find_by_id(params[:id])
+        @user = User.find_by(params[:id])
+        if logged_in? && @tweet.user_id == @user.id
+            erb :'/tweets/edit_tweet'
+        else
+            redirect '/tweets'
+        end
+    end
+    patch '/tweets/:id' do
+        @tweet = Tweet.find_by_id(params[:id])
+        @tweet.content = params[:content]
+        @tweet.save
+        redirect to "/tweets/#{@tweet.id}"
+    end
+    delete '/tweets/:id/delete' do
+        @tweet = Tweet.find_by_id(params[:id])
+        @user = User.find_by_id(params[:id])
+        if logged_in? && @tweet.user_id == @user.id
+            @tweet.destroy
+            redirect to "/tweets"
+        else
+            redirect '/login'
+        end
+            #erb :show
+   end
+end  
