b7d976a8d34fe2faa3a13bc10f2ce28e52f06a40
gabejoseph <gabriel.joseph4@gmail.com>
2020-06-18 17:51:42 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,11 @@ require 'pry'
 class TweetsController < ApplicationController
 
     get '/tweets' do 
-        erb :'/tweets/tweets'
+        if !logged_in?
+            redirect to '/login'
+        else
+            erb :'/tweets/tweets'
+        end 
     end 
     
 
