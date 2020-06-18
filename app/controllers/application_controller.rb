01d6a16a5d66bd45a0685a82ebff2b4504cae804
gabejoseph <gabriel.joseph4@gmail.com>
2020-06-18 14:00:40 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do 
+    erb :index
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
+require 'pry'
 class UsersController < ApplicationController
 
+    get '/users/create_user' do
+        binding.pry
+        erb :'/users/create_user'
+    end 
+
+    post '/users' do 
+        binding.pry
+        @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+        @user.save
+        session[:user_id] = @user.id
+    end 
 
 end
