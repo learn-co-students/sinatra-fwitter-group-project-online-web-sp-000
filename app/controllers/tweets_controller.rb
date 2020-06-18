04d1a14a43584514a00bae8c8a9b49c670a7bd4a
gabejoseph <gabriel.joseph4@gmail.com>
2020-06-18 14:21:08 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
-# require 'pry'
+require 'pry'
 class TweetsController < ApplicationController
+
+    
 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,15 @@
 require 'pry'
 class UsersController < ApplicationController
 
-    get '/users/create_user' do
-        binding.pry
+    get '/signup' do
         erb :'/users/create_user'
     end 
 
-    post '/users' do 
-        binding.pry
-        @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    post '/signup' do 
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
         @user.save
         session[:user_id] = @user.id
+        redirect to "/tweets"
     end 
 
 end
