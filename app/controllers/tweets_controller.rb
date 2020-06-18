cf673f5152c7af008ef9852641d010bd2c60f40a
Ronda D Cowen <cowenhome5@gmail.com>
2020-06-18 20:15:45 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,4 @@
 class TweetsController < ApplicationController
-<<<<<<< HEAD
-=======
-
->>>>>>> 6265492469202805c30fcd2243607474289ef005
     get '/tweets' do
       if logged_in?
         @tweets = Tweet.all
@@ -11,10 +7,6 @@ class TweetsController < ApplicationController
         redirect to '/login'
       end
     end
-<<<<<<< HEAD
-=======
-
->>>>>>> 6265492469202805c30fcd2243607474289ef005
     get '/tweets/new' do
       if logged_in?
         erb :'/tweets/new'
@@ -22,18 +14,10 @@ class TweetsController < ApplicationController
         redirect to '/login'
       end
     end
-<<<<<<< HEAD
-=======
-
->>>>>>> 6265492469202805c30fcd2243607474289ef005
     post '/tweets' do
       @tweet = Tweet.create(:content => params[:content])
       redirect to "/tweets/#{@tweet.id}"
     end
-<<<<<<< HEAD
-=======
-
->>>>>>> 6265492469202805c30fcd2243607474289ef005
     get '/tweets/:id' do
      if logged_in?
        @tweet = Tweet.find_by_id(params[:id])
@@ -42,54 +26,30 @@ class TweetsController < ApplicationController
        redirect to '/login'
      end
    end
-<<<<<<< HEAD
-=======
-
->>>>>>> 6265492469202805c30fcd2243607474289ef005
     get '/tweets/:id/edit' do
         @tweet = Tweet.find_by_id(params[:id])
         @user = User.find_by(params[:id])
         if logged_in? && @tweet.user_id == @user.id
             erb :'/tweets/edit_tweet'
-<<<<<<< HEAD
         else
-          redirect '/tweets'
+            redirect '/tweets'
         end
     end
-=======
-        else 
-            redirect '/tweets'
-        end 
-    end
-
->>>>>>> 6265492469202805c30fcd2243607474289ef005
     patch '/tweets/:id' do
         @tweet = Tweet.find_by_id(params[:id])
         @tweet.content = params[:content]
         @tweet.save
         redirect to "/tweets/#{@tweet.id}"
     end
-<<<<<<< HEAD
-=======
-
->>>>>>> 6265492469202805c30fcd2243607474289ef005
     delete '/tweets/:id/delete' do
         @tweet = Tweet.find_by_id(params[:id])
         @user = User.find_by_id(params[:id])
         if logged_in? && @tweet.user_id == @user.id
             @tweet.destroy
             redirect to "/tweets"
-<<<<<<< HEAD
         else
-            redirect '/login'
-        end
-   end
-end
-=======
-        else 
             redirect '/login'
         end
             #erb :show
    end
 end  
->>>>>>> 6265492469202805c30fcd2243607474289ef005
