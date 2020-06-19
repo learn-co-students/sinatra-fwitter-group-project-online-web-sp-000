01b3c505ee85afcdd19d60fa2be82dec4be3e97e
gabejoseph <gabriel.joseph4@gmail.com>
2020-06-19 11:01:14 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,7 +18,39 @@ class TweetsController < ApplicationController
             erb :'/tweets/new'
         end 
     end 
+
+    post '/tweets' do 
+        if !params[:content].empty?
+            @tweet = Tweet.create(content: params[:content], user_id: session[:user_id])
+            @tweet.save
+        else 
+            redirect to '/tweets/new'
+        end 
+    end 
+
+    get '/tweets/:id' do 
+        if !logged_in?
+            redirect to '/login'
+        else 
+            @tweet = Tweet.find_by(user_id: params[:id])
+            erb :'/tweets/show_tweet'
+        end 
+    end 
+
+    get '/tweets/:id/edit' do 
+        if !logged_in? 
+            redirect to '/login'
+        end 
+        @tweets = Tweet.find_by(user_id: session[:user_id])
+        # if current_user.id != @tweets.user_id
+        #     redirect to '/tweets'
+        # end
+
+        erb :'/tweets/edit_tweet'
+    end 
     
+    patch '/tweets/:id' do 
+    end 
 
 
 end
