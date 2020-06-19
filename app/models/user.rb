5096344603612da5afc2698d7a6e54786149f1ca
denalibalser <denali.balser167@gmail.com>
2020-06-18 20:48:09 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,7 @@ class User < ActiveRecord::Base
   end 
 
   def self.find_by_slug(slug)
-  
-    User.all.detect do |result|
-      result.slug === @slug
-    end
+    User.all.find{|user| user.slug == slug}
   end
 
 end
