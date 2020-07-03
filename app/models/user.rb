23119119513e9b872dc7cd86a7b87a0b4b2d4f6b
murphcoder <murphcoder@gmail.com>
2020-07-02 19:20:08 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def slug
+    title = self.username
+    slug = title.downcase
+    slug.tr!(" ", "-")
+    slug.gsub!(/[^a-zA-Z\d-]/, "")
+    slug
+  end
+
+  def self.find_by_slug(slug)
+    self.all.select {|item| item.slug == slug}.first
+  end
+
 end
