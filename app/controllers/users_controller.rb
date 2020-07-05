457bbc69153388679d62dfaa40fe19c9bf7f3c79
chienleow <chienleow1@gmail.com>
2020-07-05 14:49:56 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,9 +45,10 @@ class UsersController < ApplicationController
         end
     end
 
-    get 'logout' do
+    get '/logout' do
         if logged_in?
             session.clear
+            redirect to '/login'
         else
             redirect to '/'
         end
