18162a2319ecd9547a49acd92cd463b9908167c5
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-07-07 19:47:51 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,61 @@
 class UsersController < ApplicationController
+    enable :sessions
+    set :session_secret, 'fwitter'
+    set :public_folder, 'public'
+    set :views, 'app/views'
 
+  #loads the signup page + creates a new user
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets' 
+    else
+      erb :'users/create'
+    end
+  end
 
+  post '/signup' do
+    @user = User.new(params)
+    if @user.save
+      session[:id] = @user.id
+      redirect to('/tweets')
+    else
+      redirect to('/signup')
+    end
+  end
+
+  #loads the login page + logs in the user
+    get '/login' do
+      if logged_in?
+        redirect to('tweets') 
+      else  
+        erb :'users/login'
+      end
+    end
+
+    post '/login' do
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:id] = @user.id
+        redirect to("/tweets")
+      else
+        erb :'users/login'
+      end
+    end
+
+  #Logs out the user
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to('/login')
+    else
+      redirect to('/')
+    end
+  end
+
+  #Displays individual user's tweets
+  get '/users/:slug' do
+    @tweet = Tweet.find_by(params[:id])
+    @user = User.find_by_slug(params[:slug])
+    erb :"tweets/tweets"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+  validates_presence_of :username, :email, :password
+
+  validates :username, uniqueness: true
+
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
 end
