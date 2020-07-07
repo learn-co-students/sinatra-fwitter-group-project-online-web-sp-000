44d8918c1ec40253d9286c983d8e6611b70fbaf4
wkdewey <wkdewey@gmail.com>
2020-07-07 19:42:05 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,66 @@
 class TweetsController < ApplicationController
 
 
+  get '/tweets' do
+    if session[:user_id]
+      @user = Helpers.current_user(session)
+      @tweets = Tweet.all
+      erb :"tweets/index"
+    else
+      
+      redirect "/login"
+    end
+  end
+
+  get '/tweets/new' do
+    if Helpers.is_logged_in?(session)
+      erb :"tweets/new"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/tweets/:id' do
+    if Helpers.is_logged_in?(session)
+      @tweet = Tweet.find_by(id: params[:id])
+      erb :"tweets/show"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by(id: params[:id])
+    if Helpers.is_logged_in?(session) && Helpers.current_user(session) == @tweet.user
+      erb :"tweets/edit"
+    else
+      redirect "/login"
+    end
+  end
+
+  post '/tweets' do
+    tweet = Tweet.new(content: params[:content], user_id: Helpers.current_user(session).id)
+    if tweet.save
+      redirect "/tweets"
+    else
+      redirect "/tweets/new"
+    end
+  end
+
+  post '/tweets/:id/edit' do
+    tweet = Tweet.find_by(id: params[:id])
+    tweet.content = params[:content]
+    if tweet.save
+      redirect "/tweets"
+    else
+      redirect "/tweets/#{tweet.id}/edit"
+    end
+  end
+  
+  delete '/tweets/:id' do
+    if Helpers.current_user(session) == Tweet.find_by(id: params[:id]).user
+      tweet = Tweet.find_by(id: params[:id])
+      tweet.destroy
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
 
+  
+  get '/users/:slug' do
+    @tweets = User.find_by_slug(params[:slug]).tweets
+    erb :"users/show"
+  end
+
+
 
 end
