d798d61e66ae0672c920d9e34943516c5202616e
Gearz <shanegeary@yahoo.com>
2020-07-08 16:49:10 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,17 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "geometrical"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
+  get '/' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    else
+      erb :index
+    end 
+  end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,69 @@
 class TweetsController < ApplicationController
+    get '/tweets' do
+        if Helpers.is_logged_in?(session)
+            @tweets = Tweet.all
+            erb :'/tweets/tweets'
+        else
+            redirect to '/login'
+        end
+    end
 
+    get '/tweets/new' do
+        if Helpers.is_logged_in?(session)
+            erb :'/tweets/new'
+        else
+            redirect to '/login'
+        end
+    end
 
+    get '/tweets/:id' do
+        if Helpers.is_logged_in?(session)
+            @tweet = Tweet.find(params[:id])
+            erb :'/tweets/show'
+        else
+            redirect to '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        if Helpers .is_logged_in?(session) && Helpers.current_user(session) == Tweet.find(params[:id]).user
+            @tweet = Tweet.find(params[:id])
+            erb :'/tweets/edit'
+        else
+            redirect to '/login'
+        end
+    end
+
+    post '/tweets' do
+        if params[:content].empty?
+            redirect to '/tweets/new'
+        else
+            tweet = Tweet.new
+            tweet.content = params[:content]
+            tweet.user = Helpers.current_user(session)
+            tweet.save
+            redirect to "/tweets/#{tweet.id}"
+        end
+    end
+
+    patch '/tweets/:id' do
+        if !params[:content].empty?
+            tweet = Tweet.find(params[:id])
+            tweet.content = params[:content]
+            tweet.user = Helpers.current_user(session)
+            tweet.save
+            redirect to "/tweets/#{tweet.id}"
+        else
+            redirect to "/tweets/#{params[:id]}/edit"
+        end
+    end
+
+    delete '/tweets/:id' do
+        if Helpers.current_user(session) == Tweet.find(params[:id]).user
+            Tweet.delete(params[:id])
+            redirect to "/"
+        else
+            redirect to "/tweets/#{params[:id]}/edit"
+        end
+    end
 end
