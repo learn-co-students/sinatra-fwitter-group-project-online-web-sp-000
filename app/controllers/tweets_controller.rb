2744d71e153659390bf3d62d6cb98543c2214d4e
jackcooper1245 <jackcooper1245@gmail.com>
2020-07-11 22:28:08 +0200

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,21 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        erb :'/tweets/tweets'
+    end
 
+    get '/tweets/new' do
+        @tweets = Tweet.all
+        erb :'/tweets/new'
+    end
+
+    get '/tweets/:id' do
+        @tweet = Tweet.find_by(id: params[:id])
+        erb :'/tweets/show'
+    end
+
+    post '/tweets' do
+        @tweet = Tweet.new(content: params[:content])
+        redirect '/tweets/tweets'
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        erb :'/users/create_user'
+    end
 
+    post '/signup' do
+        @user = User.new
+        @user.username = params[:username]
+        @user.email = params[:email]
+        @user.password = params[:password]
+        redirect '/tweets/tweets'
+    end
 end
