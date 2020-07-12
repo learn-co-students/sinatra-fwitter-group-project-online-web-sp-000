b441b1e741e2d3d3df7b636a2357fbbb75e8f6a5
clincoln12 <chris.lincoln12@gmail.com>
2020-07-12 09:54:40 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,50 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+        redirect to '/tweets'
+    else
+        erb :'users/signup'
+    end
+  end
+
+
+  post '/signup' do
+    @user = User.new(params)
+    if @user.save
+        session[:user_id] = @user.id
+        redirect "/tweets"
+    else
+        redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+        erb :'users/login'
+    else
+        redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect '/tweets'
+    else
+        redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
 
 end
