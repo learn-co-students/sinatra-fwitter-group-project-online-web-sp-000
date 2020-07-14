873c737146b2469ed1f6ade6b4b93658dd812117
ting682 <tchung682@gmail.com>
2020-07-14 13:34:38 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  validates :username, :presence => true
+  validates :email, :presence => true
+  validates :password, :presence => true
+
+  def slug  
+    self.username.split(" ").map do |word|
+        word.downcase 
+    end.join("-")
+  end
+
+  def self.find_by_slug(slug)
+    @name = slug.split("-").map do |word|       
+            word
+    end.join(" ")
+    #binding.pry
+    User.find_by(username: @name) 
+  end
 end
