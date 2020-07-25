7f36a997058d64ca57aca2056351f1b323f9b14f
acg2176 <acg2176@columbia.edu>
2020-07-25 15:52:06 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,20 @@
+require './config/environment'
+require 'sinatra/base'
 class TweetsController < ApplicationController
+   register Sinatra::ActiveRecordExtension
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "password_security"
+    end
 
-
+   get '/tweets' do
+      if !Helpers.is_logged_in?(session)
+         redirect to '/login'
+      end
+      @tweets = Tweet.all
+      @user = Helpers.current_user(session)
+      erb :'/tweets/tweets'
+   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
+require 'rack-flash'
+require 'sinatra/base'
+
 class UsersController < ApplicationController
+    enable :sessions
+    use Rack::Flash
+    register Sinatra::ActiveRecordExtension
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "password_security"
+    end
 
+    get '/login' do
+        if Helpers.is_logged_in?(session)
+            redirect to '/tweets'
+        end
+      
+        erb :'/users/login'
+    end
 
+    post '/login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect to '/tweets'
+        else
+            flash[:message] = "Incorrect login. Please try again."
+            redirect to '/login'
+        end
+    end
 end
