26133b78806ff1165594bbc0ceeed7725d18f525
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-26 23:31:37 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
-  has_secure_password
   has_many :tweets
+  validates_presence_of :username, :email, :password
+  has_secure_password
+
+  include Slug
+  extend FindBySlug
 end
