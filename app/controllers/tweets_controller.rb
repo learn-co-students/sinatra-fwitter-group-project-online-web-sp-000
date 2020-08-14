ef9f53c373b0ff6abac315771c523298e6c7b0f8
Carla Holcomb <carla.programmer@gmail.com>
2020-08-14 14:36:21 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   get '/tweets' do
     if logged_in?
       @tweets = Tweet.all
-      erb :'tweets/tweets'
+      redirect to '/index'
     else
       redirect to '/login'
     end
