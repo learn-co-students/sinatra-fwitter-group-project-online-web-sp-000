49b2e3d70d8dcc1efe76522cdd3ccef084e4bfe6
NicholasR77 <nl7robbins@icloud.com>
2020-08-16 13:10:15 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,78 @@
 class TweetsController < ApplicationController
+    get '/tweets/new' do
+        if (logged_in? == true)
+            erb :'/tweets/new'
+        else
+            redirect '/users/login'
+        end
+    end
 
+    post '/tweets' do
+        tweet = Tweet.new(content: params[:content], user_id: current_user.id)
+        if (params[:content] == "" || logged_in? == false)
+            redirect '/tweets/failure'
+        elsif (tweet.save)
+            redirect '/tweets'
+        end
+    end
 
+    get '/tweets' do
+        if (logged_in? == true)
+            @tweets = Tweet.all
+            erb :'/tweets/index'
+        else
+            redirect '/users/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        @tweet = Tweet.find(params[:id])
+        if (logged_in? == true && current_user.id == @tweet.user_id)
+            erb :'/tweets/edit'
+        elsif (logged_in? == false)
+            redirect '/users/login'
+        else
+            redirect '/tweets/failure' 
+        end
+    end
+
+    patch '/tweets/:id' do
+        tweet = Tweet.find(params[:id])
+        if (logged_in? == true && current_user.id == tweet.user_id)
+            tweet.content = params[:content]
+            tweet.save
+            redirect '/tweets'
+        elsif (logged_in? == false)
+            redirect '/users/login'
+        else
+            redirect '/tweets/failure' 
+        end
+    end
+
+    delete '/tweets/:id' do
+        tweet = Tweet.find(params[:id])
+        if (logged_in? == true && current_user.id == tweet.user_id)
+            tweet.delete
+            redirect '/tweets'
+        elsif (logged_in? == false)
+            redirect '/users/login'
+        else
+            redirect '/tweets/failure' 
+        end
+    end
+
+    get '/tweets/:id' do
+        @tweet = Tweet.find(params[:id])
+        if (logged_in? == true && current_user.id == @tweet.user_id)
+            erb :'/tweets/show'
+        elsif (logged_in? == false)
+            redirect '/users/login'
+        else
+            redirect '/users/failure' 
+        end
+    end
+
+    get '/tweets/failure' do
+        erb :'/tweets/failure'
+    end
 end
