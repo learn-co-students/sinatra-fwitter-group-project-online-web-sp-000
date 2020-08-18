337b59257e959af851c101407969b4689875b7da
Dan-zai <danielcowpertwait@gmail.com>
2020-08-18 16:37:46 +1000

    Final Build

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,25 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, "ooh so secret"
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :home
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
   end
 
 end
---
+++ app/views/users/signup.erb
@@ -0,0 +1,7 @@
+<h3>Sign Up</h3>
+<form action="/signup" method="POST">
+  <p>Name: <input type="text" name="username"></p>
+  <p>Email: <input type="text" name="email"></p>
+  <p>Password: <input type="text" name="password"></p>
+  <input class="btn btn-primary" type="submit">
+</form>
