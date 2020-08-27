6a167e4c77bc312bcb2fdfe8d5c07083f2381ceb
github username <github email address>
2020-08-27 13:03:09 -0600

    adds 3 methods to users controller:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
 
+    get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :'users/show'
+    end
+
+    get '/signup' do
+      if !logged_in?
+        erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+      else
+        redirect to '/tweets'
+      end
+    end
+
+    post '/signup' do
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect to '/signup'
+      else
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        @user.save
+        session[:user_id] = @user_id
+        redirect to '/tweets'
+      end
+    end
+
+    
 
 end
