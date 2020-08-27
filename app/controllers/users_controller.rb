90975bf2a0367df1a16767157a79854b848ead70
github username <github email address>
2020-08-27 13:11:11 -0600

    completes users controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,30 @@ class UsersController < ApplicationController
       end
     end
 
-    
+    get '/login' do
+      if !logged_in?
+        erb :'users/login'
+      else
+        redirect to '/tweets'
+      end
+    end
 
+    post '/login' do
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect to '/tweets'
+      else
+        redirect to '/signup'
+      end
+    end
+
+    get '/logout' do
+      if logged_in?
+        session.destroy
+        redirect to '/login'
+      else
+        redirect to '/'
+      end
+    end
 end
