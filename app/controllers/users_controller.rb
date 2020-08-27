b7da8b3d2123f43bf4ade974c182553f060ddf5f
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-27 07:55:27 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,52 @@
 class UsersController < ApplicationController
-
-  get '/signup' do # signup link in home page
-    erb :"/users/create_user"
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
   end
 
-  post '/signup' do # did you do the sign up link?
-    if logged_in?
-      redirect "/tweets"
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
     else
-      @user = User.create(params)
+      redirect to '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
       session[:user_id] = @user.id
-      redirect "/tweets/index"
+      redirect to '/tweets'
     end
   end
 
   get '/login' do
-    erb :'/users/login'
-  end
-
-  post '/login' do
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/tweets"
+    if !logged_in?
+      erb :'users/login'
     else
-      redirect '/login'
+      redirect to '/tweets'
     end
   end
 
-  get '/users/:id' do
-    @user = User.find_by(id: params[:id])
-    @tweets = Tweet.all
-    redirect :"/users/show"
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    else
+      redirect to '/signup'
+    end
   end
 
   get '/logout' do
-    session.clear
-    redirect '/login'
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
   end
-
 end
