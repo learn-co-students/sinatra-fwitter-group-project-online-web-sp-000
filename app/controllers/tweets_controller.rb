e87b5714d0a6c3c12ada8d2430dd4207ac304d9a
github username <github email address>
2020-08-28 08:15:53 -0600

    completes tweet controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -30,7 +30,7 @@ class TweetsController < ApplicationController
         end
       end
     else
-    redirect to '/login'
+      redirect to '/login'
     end
   end
 
@@ -43,14 +43,14 @@ class TweetsController < ApplicationController
     end
   end
 
-  get 'tweets/:id/edit' do
+  get '/tweets/:id/edit' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
       if @tweet && @tweet.user == current_user
         erb :'tweets/edit_tweet'
-      else
-        redirect to '/tweets'
-      end
+        else
+          redirect to '/tweets'
+        end
       else
         redirect to '/login'
     end
@@ -65,19 +65,27 @@ class TweetsController < ApplicationController
           if @tweet && @tweet.user == current_user
             if @tweet.update(content: params[:content])
               redirect to "/tweets/#{@tweet.id}"
-           else
-            redirect to "/tweets/#{@tweet.id}/edit"
+            else
+              redirect to "/tweets/#{@tweet.id}/edit"
             end
           else
             redirect to '/tweets'
+          end
         end
-      end
     else
       redirect to '/login'
     end
   end
 
-  
-
-
+  delete '/tweets/:id/delete' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+        if @tweet && @tweet.user == current_user
+          @tweet.delete
+        end
+        redirect to '/tweets'
+      else
+        redirect to '/login'
+      end
+  end
 end
