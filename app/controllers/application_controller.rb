c872db1004edd1f0a5e432bfadad76012eb950af
github username <github email address>
2020-08-28 21:13:42 -0600

    adds tweets.erb and corrects erb file names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,20 +7,21 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "fwitter_secret"
+  end
 
-    get '/' do
-      erb :index
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
     end
 
-    helpers do
-
-      def logged_in?
-        !!current_user
-      end
-
-      def current_user
-        @current_user ||=User.find_by(id: session[:user_id]) if session[:user_id]
-      end
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
+
   end
 end
