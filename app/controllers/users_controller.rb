6db24086b46500bb928e899b561543d3ed19d0ba
github username <github email address>
2020-09-02 15:45:08 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
       else
         @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
         @user.save
-        session[:user_id] = @user_id
+        session[:user_id] = @user.id
         redirect to '/tweets'
       end
     end
