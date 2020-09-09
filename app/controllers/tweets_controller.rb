66198d75824d7136e4d543fa7d89e4aea134bc7c
Snowing222 <snowing222@gmail.com>
2020-09-09 16:35:24 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,53 @@
 class TweetsController < ApplicationController
+    get '/tweets' do
+        if logged_in?
+            @tweets = Tweet.all
+            @user = User.find(session[user_id])
+            erb :'tweets/tweets'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/tweets/new' do
+        if logged_in?
+            erb :'tweets/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/tweets' do
+        tweet = Tweet.new(params)
+        if tweet.save
+            redirect "/tweets/#{tweet.id}"
+        else
+            redirect '/tweets/new'
+        end
+
+    end
+
+    get '/tweets/:id' do
+        if logged_in?
+            erb :'tweets/show_tweet'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        if logged_in?
+            erb :'tweets/edit_tweet'
+        else
+            redirect '/login'
+        end
+    end
+
+    patch '/tweets/:id' do
+        tweet = Tweet.update(params)
+        redirect "/tweets/#{tweet.id}"
+
+    end
 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+    get '/signup' do
+        erb :'/users/create_user'
+    end
+
+    post '/signup' do
+        user = User.new(params)
+        if User.find_by(email: params[:email])
+            redirect '/login'
+        elsif user.save
+            session[:user_id] = user.id
+            redirect '/login'
+        else
+            redirect '/signup'
+        end
+    end
+
+    get '/login' do
+        
+
+    end
+ 
+
 
 
 end
