4040e3c37ccfb491c9ee59bf94ab436040e933af
carriemathieu <carrieburks@gmail.com>
2020-09-12 19:25:11 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,10 @@
 class TweetsController < ApplicationController
 
-
+    get '/tweets' do
+        #binding.pry
+        @tweets = Tweet.all
+        erb :"tweets/tweets"
+        #if user is logged in-> direct to tweets/tweets
+        #if not logged in -> direct to login page users/login
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,39 @@
 class UsersController < ApplicationController
 
+get '/signup' do
+    erb :'users/create_user'
+end
+
+post '/signup' do
+    #binding.pry
+    # if user is not logged in, cannot view page
+    if params[:email].empty? || params[:username].empty? ||params[:password].empty?
+        redirect to '/signup'
+    end
+    User.create(email: params[:email], username: params[:username], password: params[:password])
+    redirect to '/tweets'
+end
+
+get '/login' do
+    erb :'users/login'
+end
+
+post '/login' do
+    binding.pry
+    @user = User.find_by(:username => params[:username])
+    
+    if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        @user = @user
+        redirect to '/tweets'
+    else
+        redirect to '/login'
+    end  
+end
+
+get '/logout' do
+    session.clear
+    redirect '/'
+end
 
 end
