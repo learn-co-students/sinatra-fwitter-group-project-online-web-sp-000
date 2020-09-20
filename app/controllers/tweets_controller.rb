2239de04e25deffd2b6a24312d9c4b9750624719
zacharytq <zacharytq@gmail.com>
2020-09-20 19:35:02 -0400

    problems with tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,17 +3,20 @@ class TweetsController < ApplicationController
     get '/tweets' do
         if logged_in?
             @tweets = Tweet.all
-            @users = User.all
             erb :'/tweets/all'
         else
             redirect to '/login'
         end
     end
-
-    get '/tweets/:id' do 
-        @tweet = Tweet.find[:id]
-        erb :'/tweets/show_tweet'
+    
+    get '/tweets/new' do
+        if logged_in?
+            erb :"/tweets/new"
+        else
+            redirect to '/login'
+        end
     end
 
+    
 
 end
