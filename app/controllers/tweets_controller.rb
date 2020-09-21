2a27938920d113c36d1df984aa645227067f5f65
zacharytq <zacharytq@gmail.com>
2020-09-21 18:44:52 -0400

    login weirdness

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,6 +17,47 @@ class TweetsController < ApplicationController
         end
     end
 
+    post "/tweets/new" do
+        @user = current_user
+        @tweet = Tweet.create(content: params[:content], user: @user)
+        
+        if @tweet.save
+            redirect to "/users/#{@user.slug}"
+        else
+            redirect to '/tweets/new'
+        end
+    end
+
+    get "/tweets/:id" do
+        if logged_in?
+            @tweet = Tweet.find(params[:id])
+            erb :'/tweets/show_tweet'
+        else
+            redirect to "/login"
+        end
+    end
+
+    get "/tweets/:id/edit" do
+        @tweet = Tweet.find(params[:id])
+        if logged_in?
+            erb :'/tweets/edit_tweet'
+        else
+            redirect to '/login'
+        end
+    end
+
+    patch "/tweets/:id" do
+        @tweet = Tweet.find(params[:id])
+        @user = current_user
+        if @tweet.update(content: params[:content], user: @user)
+            redirect to "/tweets/#{@tweet.id}"
+        else
+            redirect to "/tweets/#{@tweet.id}/edit"
+        end
     
+    end
+
+    delete "/tweets" do
+    end
 
 end
