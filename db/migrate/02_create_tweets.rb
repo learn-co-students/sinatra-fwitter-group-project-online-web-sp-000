85d8ab2915de7064ffcd6fa9e5e3b657eacd72bf
reevesalic <reevesalic@gmail.com>
2020-09-29 16:57:14 -0400

    Done.

--- db/migrate/02_create_tweets.rb
+++ db/migrate/02_create_tweets.rb
@@ -3,8 +3,7 @@ class CreateTweets < ActiveRecord::Migration[5.2]
        create_table :tweets do |t|
          t.text :content
          t.integer :user_id
-   
-     #     t.timestamps null: false
+         t.timestamps null: false
        end
      end
    end
