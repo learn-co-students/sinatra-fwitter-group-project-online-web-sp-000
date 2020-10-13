202d0ca1d8d7731f949b5264a25f98747a3c462e
milos381 <milos_popovic@outlook.com>
2020-10-13 14:29:40 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,182 @@
 class TweetsController < ApplicationController
+    get '/tweets' do
+      if logged_in?
+        @tweets = Tweet.all
+        erb :'tweets/tweets'
+      else
+        redirect to '/login'
+      end
+    end
+  
+    get '/tweets/new' do
+      if logged_in?
+        erb :'tweets/create_tweet'
+      else
+        redirect to '/login'
+      end
+    end
+  
+    post '/tweets' do
+      if logged_in?
+        if params[:content] == ""
+          redirect to "/tweets/new"
+        else
+          @tweet = current_user.tweets.build(content: params[:content])
+          if @tweet.save
+            redirect to "/tweets/#{@tweet.id}"
+          else
+            redirect to "/tweets/new"
+          end
+        end
+      else
+        redirect to '/login'
+      end
+    end
+  
+    get '/tweets/:id' do
+      if logged_in?
+        @tweet = Tweet.find_by_id(params[:id])
+        erb :'tweets/show_tweet'
+      else
+        redirect to '/login'
+      end
+    end
+  
+    get '/tweets/:id/edit' do
+      if logged_in?
+        @tweet = Tweet.find_by_id(params[:id])
+        if @tweet && @tweet.user == current_user
+          erb :'tweets/edit_tweet'
+        else
+          redirect to '/tweets'
+        end
+      else
+        redirect to '/login'
+      end
+    end
+  
+    patch '/tweets/:id' do
+      if logged_in?
+        if params[:content] == ""
+          redirect to "/tweets/#{params[:id]}/edit"
+        else
+          @tweet = Tweet.find_by_id(params[:id])
+          if @tweet && @tweet.user == current_user
+            if @tweet.update(content: params[:content])
+              redirect to "/tweets/#{@tweet.id}"
+            else
+              redirect to "/tweets/#{@tweet.id}/edit"
+            end
+          else
+            redirect to '/tweets'
+          end
+        end
+      else
+        redirect to '/login'
+      end
+    end
+  
+    delete '/tweets/:id/delete' do
+      if logged_in?
+        @tweet = Tweet.find_by_id(params[:id])
+        if @tweet && @tweet.user == current_user
+          @tweet.delete
+        end
+        redirect to '/tweets'
+      else
+        redirect to '/login'
+      end
+    end
+  end
+# require 'pry'
+# class TweetsController < ApplicationController
+      
+#     get '/tweets' do # index action allowing view to access all recipes thru instance variable @recipes
+#         if logged_in?
+#             @tweets = current_user.tweets
+#             erb :'/tweets/tweets'
+#         else
+#             erb :'/'
+#         end
+#       end
 
+#     get '/tweets/new' do # loads form to create new recipe
+        
+#         if logged_in?
+#             erb :'/tweets/create_tweet'
+#         else
+#             redirect to '/login'
+#         end
+#     end
+    
+#     post '/tweets' do
+#         if logged_in?
+#           if params[:content] == ""
+#             redirect to "/tweets/new"
+#           else
+#             @tweet = current_user.tweets.build(content: params[:content])
+#             if @tweet.save
+#               redirect to "/tweets/#{@tweet.id}"
+#             else
+#               redirect to "/tweets/new"
+#             end
+#           end
+#         else
+#           redirect to '/login'
+#         end
+#       end
+    
+#     get '/tweets/:id' do # show action to display a single recipe. dynamic url allows ID to be called in the view thru params hash
+#         if logged_in?
+#             @tweet = Tweet.find_by_id(params[:id])
+#             erb :'tweets/show_tweet'
+#         else
+#             redirect to '/login'
+#         end
+#     end
+    
+#     get '/tweets/:id/edit' do #load edit form
+#         if logged_in?
+#             #binding.pry
+#             @tweet = current_user.tweets.find_by_id(params[:id])
+#             erb :'tweets/edit_tweet'
+#         else
+#             redirect to '/login'
+#         end
+#     end
+    
+#     patch '/tweets/:id' do #edit action. edit form submission. redirect to show page.
+#         @tweet = Tweet.find_by_id(params[:id])
+#         @tweet.content = params[:content]
+#         @tweet.user_id = params[:user_id]
+#         @tweet.save
+    
+#         redirect to "/tweets/#{@tweet.id}"
+#         if logged_in?
+#             @tweet = current_user.tweets.find_by_id(params[:id])
+#             @tweet.content = params[:content]
+#             @tweet.save
+#             redirect to "/tweets/#{@tweet.id}"
+#         else
+#             redirect to '/tweets/tweets'
+#         end
+#     end
+    
 
-end
+    
+     
+    
+#     delete '/tweets/:id/delete' do
+#         if logged_in?
+#           @tweet = Tweet.find_by_id(params[:id])
+#           if @tweet && @tweet.user == current_user
+#             @tweet.delete
+#           end
+#           redirect to '/tweets'
+#         else
+#           redirect to '/login'
+#         end
+#       end
+      
+
+# end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,56 @@
 class UsersController < ApplicationController
 
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        erb :'users/show'
+      end
+    get "/signup" do
+        
+        if !logged_in?
+            erb :'/users/signup'
+          else
+            redirect to '/tweets'
+        end
+    end
+    post '/signup' do
+        
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect '/users/signup'
+        else
+            #binding.pry
+            @user = User.create(username: params[:username], password: params[:password])
+            session[:user_id] = @user.id
+            redirect '/tweets'
+        end
+    end
+    get '/login' do
+        if !logged_in?
+            erb :'users/login'
+          else
+            redirect to '/tweets'
+        end
+    end
+    post '/login' do
+        #here we need to find the user with a given username
+        #if there is such a user, we need to doublecheck it's password
+        #if the password is ok and user exists, the new session starts
+        #whose id is the same as users id
+        @user = User.find_by(:username => params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect to '/tweets'
+        else
+            redirect "/users/signup"
+        end
+    end
+    
+    get "/logout" do
+        if logged_in?
+            session.destroy
+            redirect to '/login'
+        else
+            redirect to '/'
+        end
+    end
 
 end
