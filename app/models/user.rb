e9c34305fb7e2446dd617929089a96b71a7705f2
khoinguyenkc <khoinguyenkc@gmail.com>
2020-10-13 14:21:04 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+
+  def slug
+    namesplittoarray  = self.username.downcase.split(" ")
+    slug = namesplittoarray.join("-")
+
+  end
+
+  def self.find_by_slug(theslug)
+    user = User.all.find do | user |
+      user.slug == theslug
+    end
+    #we have to use find method, not find_by, becaue .slug is not an attribute, but a method
+  end
+
 end
