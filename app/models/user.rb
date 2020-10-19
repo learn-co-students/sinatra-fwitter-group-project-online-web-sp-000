277034b113a19e20908262e220cad833928ae04d
github username <github email address>
2020-10-18 20:29:28 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
+  
+  #Added for module slugify
+  require_relative './slugify.rb'
+  require_relative './helpers.rb'
+  include Slugify::InstanceMethod
+  include Helpers::InstanceMethod
+  extend Slugify::ClassMethod
+  extend Helpers::ClassMethod
+  #----
+
+  #Added dependencies
   has_secure_password
   has_many :tweets
+  #----
+  
 end
+  
