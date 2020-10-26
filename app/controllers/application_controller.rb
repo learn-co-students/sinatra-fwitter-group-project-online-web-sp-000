3022ebe48abeced6fae10a0fe7234017821f6442
dsulliv4 <dsulliv4@binghamton.edu>
2020-10-25 20:59:29 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
   end
 
+
+  get '/' do 
+    erb :index 
 end
+
+end 
