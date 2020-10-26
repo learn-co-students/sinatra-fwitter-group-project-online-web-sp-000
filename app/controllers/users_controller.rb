3846d347e519e675c2a93908bc38635524c604fc
Yvonne <yvonnersz@gmail.com>
2020-10-26 11:52:13 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,54 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if logged_in?
+          redirect '/tweets'
+        else
+          erb :'/users/signup'
+        end
+      end
+    
+      post '/signup' do
+        if params[:username].empty? || params[:email].empty? || params[:password].empty?
+          redirect '/signup'
+        else
+          @user = User.create(params)
+          session[:user_id] = @user.id
+          redirect '/tweets'
+        end
+      end
+
+    get '/login' do
+        if logged_in?
+            redirect '/tweets'
+        else
+            erb :'/users/login'
+        end
+    end
+
+    post '/login' do
+        @user = User.find_by(:username => params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect '/tweets'
+        else
+            redirect '/signup'
+        end
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.clear
+            redirect '/login'
+        else
+            redirect '/tweets'
+        end
+    end
+
+    get '/users/:username' do
+        @user = User.find_by(:username => params[:id])
+        erb :'/users/show'
+    end
 
 end
