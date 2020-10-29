a4e55025aabc5d8290334b450f41736220b837f0
jrog20 <jaimerogstad@gmail.com>
2020-10-29 19:45:35 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
 
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/tweets/tweets'
+    end
+  end
 
 end
