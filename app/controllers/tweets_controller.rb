3d143be7c6bd11d88a0d3904934b8487baf067c2
Joseph Perez <hello@josephdanielperez.com>
2020-10-29 22:27:54 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,97 @@
+require './config/environment'
+
 class TweetsController < ApplicationController
+  configure do
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'secret'
+  end
 
+  get '/tweets' do
+    if User.logged_in?(session)
+      @user = User.current_user(session)
+      @tweets = Tweet.all
 
+      erb :'/tweets/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if User.logged_in?(session)
+      erb :'/tweets/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if !User.logged_in?(session)
+      redirect to '/login'
+    end
+
+    @tweet = Tweet.find_by(id: params[:id])
+    @user = User.current_user(session)
+
+    if @tweet.user.id = @user.id
+      erb :'/tweets/edit_tweet'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  get '/tweets/:id' do
+    if User.logged_in?(session)
+      @user = User.current_user(session)
+      @tweet = Tweet.find_by(id: params[:id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    if User.logged_in?(session)
+      user = User.current_user(session)
+      tweet = Tweet.new(content: params[:content])
+      tweet.user = user
+      if tweet.save
+        redirect to "/tweets/#{tweet.id}"
+      else
+        redirect to '/tweets/new'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id/edit' do
+    if User.logged_in?(session)
+      user = User.current_user(session)
+      tweet = Tweet.find_by(id: params[:id])
+
+      if tweet && user.id == tweet.user.id
+        tweet.content = params[:content]
+        if !tweet.save
+          redirect to "/tweets/#{tweet.id}/edit"
+        end
+      end
+    end
+
+    redirect to '/tweets'
+  end
+
+  delete '/tweets/:id/delete' do
+    if User.logged_in?(session)
+      user = User.current_user(session)
+      tweet = Tweet.find_by(id: params[:id])
+
+      if tweet && user.id == tweet.user.id
+        tweet.destroy
+      end
+    end
+
+    redirect to '/tweets'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,26 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password_digest, presence: true
+
+  def slug
+    username.gsub(/\s/, "-")
+  end
+
+  def self.find_by_slug(slug)
+    username = slug.gsub(/-/, " ")
+    User.find_by(username: username)
+  end
+
+  def self.logged_in?(session)
+    !!session[:user_id]
+  end
+
+  def self.current_user(session)
+    User.find_by(id: session[:user_id])
+  end
 end
