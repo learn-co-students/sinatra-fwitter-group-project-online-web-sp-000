fcc766e9833cab6f5688563a549b26749b4bb5fe
codyalvarez <jedi.alvarez805@gmail.com>
2020-10-29 23:24:01 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    "Welcome to Fwitter"
+  end
+
+
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,14 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    @tweets = Tweet.all 
+    if logged_in?
+      @user = current_user
 
+      erb :'/tweets/index'
+    else
+      redirect to 'login'
+    end
+  end
+ 
 end
