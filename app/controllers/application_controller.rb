e71751304027206b08d52cd1b8fc519383d44bc0
codyalvarez <jedi.alvarez805@gmail.com>
2020-10-30 10:35:39 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,35 +5,22 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
+
+  enable :sessions
 
   get '/' do
     "Welcome to Fwitter"
   end
 
-  get '/signup' do
-    # 'signup directs user to twitter index'
-    redirect to "/tweets/#{@tweet.id}"
+  def current_user
+    User.find_by(id: session[:user_id])
   end
 
-  post '/signup' do
-    # 'does not let a user sign up without a username'
-    # does not let a user sign up without an email'
-    # 'does not let a user sign up without a password'
-    # 'does not let a logged in user view the signup page'
+  def logged_in?
+    !!current_user
   end
-
-  get '/login' do
-    # 'loads the tweets index after login'
-  end
-
-  post '/login' do
-    # 'does not let user view login page if already logged in'
-  end
-
-  get '/logout' do
-    # "lets a user logout if they are already logged in and redirects to the login page"
-  end
-
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,30 @@
 class UsersController < ApplicationController
 
+  get '/signup' do #display the user signup
+    if !logged_in?
+      erb :'users/signup'
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/signup' do
+    # 'does not let a user sign up without a username'
+    # does not let a user sign up without an email'
+    # 'does not let a user sign up without a password'
+    # 'does not let a logged in user view the signup page'
+  end
+
+  get '/login' do
+    # 'loads the tweets index after login'
+  end
+
+  post '/login' do
+    # 'does not let user view login page if already logged in'
+  end
+
+  get '/logout' do
+    # "lets a user logout if they are already logged in and redirects to the login page"
+  end
+
 end
