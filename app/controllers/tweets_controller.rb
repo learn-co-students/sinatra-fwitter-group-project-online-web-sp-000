5ae394bf990e4a71a9fb0076d5e010e92e52b1ea
codyalvarez <jedi.alvarez805@gmail.com>
2020-10-31 20:25:09 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,21 +7,48 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/new' do  
-    
+    authenticate
+    erb :'tweets/new'
   end
 
-  # post '/tweets' do
-      
-  # end
+  post '/tweets' do
+    authenticate
+    @tweet = Tweet.new(content: params[:content], user: current_user)
+    if @tweet.save
+      redirect '/tweets'
+    else
+      @error = "Cannot Create Tweet. Try Again."
+      redirect '/tweets/new'
+    end
+  end
 
-  # get '/tweets/:id' do
-    
-  # end
+  get '/tweets/:id' do
+    authenticate
+    @tweet = Tweet.find_by(id: params[:id])
+    erb :'/tweets/show_tweet'
+  end
 
-  # post '/tweets/:id/delete' do
-    
-  # end
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by(id: params[:id])
+    authorize(@tweet)
+    erb :'/tweets/edit_tweet'
+  end
 
- 
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by(id: params[:id])
+    authorize(@tweet)
+    if @tweet.update(content: params[:content])
+      redirect '/tweets'
+    else
+      redirect '/tweets/#{@tweet.id}/edit'
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find_by(id: params[:id])
+    authorize(@tweet)
+    @tweet.destroy
+    redirect '/tweets'
+  end
  
 end
