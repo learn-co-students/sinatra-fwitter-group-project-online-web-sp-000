8e6c750b77b2b31f521131e37de698e885e6ad3c
Arodriguez1030 <arodriguez1030@gmail.com>
2020-11-05 06:25:35 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,23 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter"
   end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do 
+    def current_user
+      User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
+
 
 end
