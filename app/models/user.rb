8e4d7e3e0aad97412098f39e62437027f461ac2d
Morgan Matney <morgan.matney89@gmail.com>
2020-12-13 17:20:28 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+  validates :username, presence: true
+  validates :email, presence: true
+
+  def slug
+    self.username.downcase.tr(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find do |instance|
+      instance.slug == slug
+    end
+  end
 end
