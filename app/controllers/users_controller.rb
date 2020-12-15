ffb2d4e831afada6275485530299060458b269bd
marquesavery <averymarques4@gmail.com>
2020-12-15 14:07:38 -0800

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,63 @@
+require 'rack-flash'
+
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if logged_in?
+            redirect "/tweets"
+        end
+
+        erb :'/users/signup'
+    end
+
+    post "/signup" do
+        if logged_in?
+            redirect "/tweets"
+        end
+
+        user = User.new(:email => params[:email], :username => params[:username], :password => params[:password])    
+        if user.save
+            session[:user_id] = user.id
+            redirect "/tweets"
+        else
+            flash[:message] = "There was a problem. Please try again."
+            redirect "/signup"
+        end
+    end
+
+    get "/login" do
+        if logged_in?
+            redirect "/tweets"
+        end
+
+        erb :'/users/login'
+    end
+    
+    post "/login" do
+        if logged_in?
+            redirect "/tweets"
+        end
+        
+        user = User.find_by(:username => params[:username])
+    
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/tweets"
+        else
+            flash[:message] = "There was a problem. Please try again."
+            redirect "/login"
+        end
+    end
+
+    get "/logout" do
+        session.clear
+        redirect "/login"
+    end
+
+    get '/users/:slug' do
+        # binding.pry
+        @user = User.find_by_slug(params[:slug])
+        erb :'/users/show'  
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  validates_presence_of :username, :email
   has_secure_password
   has_many :tweets
+
+  def slug
+    username.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(username)
+      User.all.find {|u| u.slug == username}
+  end
 end
