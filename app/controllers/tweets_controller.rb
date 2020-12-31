ca4eee3fa4fd215c6247094838da4c2ac5370bde
asb1512 <andrew@olympicmedia.com>
2020-12-30 19:27:13 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,11 @@
 class TweetsController < ApplicationController
 
-
+   get '/tweets' do
+      if logged_in?
+         @user = current_user
+         erb :'/tweets/index'
+      else
+         redirect "/login"
+      end
+   end
 end
