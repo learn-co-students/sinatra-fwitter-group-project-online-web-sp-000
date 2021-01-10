a08f54c209c69ba21c79ef922d43156903b939e1
acagirici <acagirici23@gmail.com>
2021-01-10 00:36:36 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,37 @@
 class TweetsController < ApplicationController
 
-
-end
+    get('/tweets') { login_redirect; @tweets = Tweet.all; erb :'tweets/tweets' }
+    get('/tweets/new') { login_redirect; erb :'tweets/new' }
+    get('/tweets/:id') { login_redirect; @tweet = Tweet.find_by_id(params[:id]); erb :'tweets/show_tweet' }
+ 
+    get '/tweets/:id/edit' do
+      login_redirect
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/edit_tweet' if @tweet && @tweet.user_id == current_user.id
+    end
+ 
+    post '/tweets' do
+      login_redirect
+      redirect_if('tweets/new', params[:content].empty?)
+      @tweet = Tweet.new(content: params[:content])
+      @user = current_user
+      @user.tweets << @tweet
+      redirect_if("/tweets/#{@tweet.id}", @user.save)
+      redirect 'tweets/new'
+    end
+ 
+    patch '/tweets/:id' do
+      login_redirect
+      redirect_if("/tweets/#{params[:id]}/edit", params[:content].empty?)
+      @tweet = Tweet.find_by_id(params[:id])
+      redirect_if("/tweets/#{@tweet.id}", @tweet && @tweet.user == current_user && @tweet.update(content: params[:content]))
+      redirect "/tweets/#{@tweet.id}/edit"
+    end
+ 
+    delete '/tweets/:id/delete' do
+      login_redirect
+      @tweet = Tweet.find_by_id(params[:id])
+      (@tweet.destroy; redirect '/tweets') if @tweet && @tweet.user_id == current_user.id
+    end
+ 
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
 class UsersController < ApplicationController
-
-
+    get('/users/:slug') { @user = current_user; erb :'users/show' }
 end
