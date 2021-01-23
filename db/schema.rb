4993f1410aba3b58c4b17194bf23ad3718704ced
Ynda Jas <yndajas@gmail.com>
2021-01-23 07:13:53 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_01_22_223545) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
 
 end
