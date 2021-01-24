e3f19f4a3267fd4a5a0ad3d75303c1cc52d3b48a
John M. Stassen <john.stassen@gmail.com>
2021-01-23 19:56:20 -0800

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,56 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if logged_in?
+          redirect '/tweets'
+        else
+          erb :'/users/signup'
+        end
+    end
+      
+    post '/signup' do
+        
+        user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        if user.save
+            session[:user_id] = user.id
+            redirect '/tweets'
+          else
+            redirect '/signup'
+          end
+    end
+
+    get  '/login' do
+        if logged_in?
+            redirect '/tweets'
+        else
+            erb :'/users/login'
+        end
+    end
+
+    post '/login' do
+        user = User.find_by(username: params[:username])
+        if (user.username !="") && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect '/tweets'
+        else
+            redirect '/login'
+        end
+    end
+        
+    get '/logout' do
+        session.clear
+        redirect '/login'
+    end  
+    
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        if !@user.nil?
+            erb :'/users/show'
+        else
+            redirect to '/index'
+        end
+    end
+
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
+  validates :password, presence: true
+
+  def self.find_by_slug(string)
+    self.all.each do |item|
+      if item.slug == string
+        return item
+      end
+    end
+  end
+
+  def slug
+    self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
 end
