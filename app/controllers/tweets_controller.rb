0dfbc8f543e4a46d00dd54e79deb7f3dfc1b106f
deliaconstantino <dconstan92@gmail.com>
2021-02-02 20:11:59 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,38 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      @tweets = Tweet.all
+      erb :'tweets/index'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if Helpers.is_logged_in?(session)
+      erb :'tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    if params[:content].blank?
+      redirect 'tweets/new'
+    else
+      user = Helpers.current_user(session)
+      user.tweets << Tweet.new(content: params[:content])
+      redirect "/users/#{user.slug}"
+    end
+  end
+
+  get 'tweets/:id' do
+      @tweet = Tweet.find_by(params[:id])
+
+      erb :'tweets/show'
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,58 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect '/tweets'
+    else
+      erb :'users/signup'
+    end
+
+  end
+
+  post '/signup' do
+    if !params[:username].blank? && !params[:email].blank?
+      user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    else
+      redirect "/signup"
+    end
+
+    if user && user.save
+      session[:user_id] = user.id
+      redirect "/tweets"
+      else
+        redirect "/signup"
+      end
+  end
+
+  get '/login' do
+    if Helpers.is_logged_in?(session)
+      redirect '/tweets'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+      session.clear
+      redirect '/login'
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by(username: params[:slug])
+
+    erb :'users/show'
+  end
+
 
 end
