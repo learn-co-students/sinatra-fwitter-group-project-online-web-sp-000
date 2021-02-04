b2ae58e309d5f6a6b319e95f7c33ee5b3f1b8ea0
Romano Livi <romano_livi@outlook.com>
2021-02-04 02:29:27 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
-
+  
+  get "/tweets" do 
+    if session[:user_id]
+      @tweets = Tweet.all 
+      erb :"/tweets/tweets"
+    else 
+      redirect "/login"
+    end
+  end
 
 end
