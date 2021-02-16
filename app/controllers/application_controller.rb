91416fbe7a0d05246e10511b57688882fb1c51e3
JohannesHupfeld <j.hupfeld@gmail.com>
2021-02-15 21:26:05 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,49 +11,4 @@ class ApplicationController < Sinatra::Base
     get '/' do
         erb :index
     end
-
-    get '/signup' do
-        if Helpers.is_logged_in?(session)
-            redirect to '/tweets'
-        else
-            erb :'users/create_user'
-        end
-    end
-    
-    post '/signup' do
-        user = User.new(email: params[:email], username: params[:username], password: params[:password])
-        if user.email != '' && user.username != '' && user.save
-            session[:id] = user.id
-            redirect to '/tweets'
-        else
-            redirect to '/signup'
-        end
-    end
-
-    get '/login' do
-        if Helpers.is_logged_in?(session)
-            redirect to '/tweets'
-        else
-            erb :'users/login'
-        end
-    end
-    
-    post '/login' do
-        user = User.find_by(username: params[:username])
-        if user && user.authenticate(params[:password])
-            session[:id] = user.id
-            redirect to '/tweets'
-        else
-            redirect to '/login'
-        end
-    end
-
-    get '/logout' do
-        if Helpers.is_logged_in?(session)
-            session.clear
-            redirect to '/login'
-        else
-            redirect to '/'
-        end
-    end
 end
