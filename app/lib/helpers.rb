346c58eabdc14c65cbad44805c2a865af57e2b71
JohannesHupfeld <j.hupfeld@gmail.com>
2021-02-15 21:42:35 -0500

    Done.

--- app/lib/helpers.rb
+++ app/lib/helpers.rb
@@ -1,11 +1,11 @@
-# class Helpers
-#     def self.is_logged_in?(session)
-#         !!session[:id]
-#     end
+class Helpers
+    def self.is_logged_in?(session)
+        !!session[:id]
+    end
 
-#     def self.current_user(session)
-#         if self.is_logged_in?(session)
-#             User.find(session[:id])
-#         end
-#     end
-# end
+    def self.current_user(session)
+        if self.is_logged_in?(session)
+            User.find(session[:id])
+        end
+    end
+end
