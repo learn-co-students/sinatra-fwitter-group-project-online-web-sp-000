f90f7c4ab17fe7d9e0d2defae229fd9b29a16ffa
amongiove <amandamongiove@gmail.com>
2021-02-18 18:00:12 -0600

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,86 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        if logged_in?
+            @tweets = Tweet.all
+            @user = User.find_by_id(session['user_id'])
+            erb :'tweets/tweets'
+        else
+            redirect to("/login")
+        end
+    end
+
+    get '/tweets/new' do
+        if logged_in?
+            @user = User.find_by_id(session['user_id'])
+            erb :'tweets/new'
+        else
+            flash[:message] = "You Must Be Logged in to Make Tweets"
+            redirect to("/login")
+        end
+    end
+
+    post '/tweets' do
+        @user = User.find_by_id(session['user_id'])
+        if params[:content].empty?
+            flash[:message] = "You Cannot Create a Blank Tweet"
+            redirect to("/tweets/new")
+        else
+            @tweet = Tweet.create(content: params[:content], user_id: @user.id)
+        end
+        @tweet.save
+        redirect to("/tweets/#{@tweet.id}")
+    end
+
+    get '/tweets/:id' do
+        if logged_in?
+            @tweet = Tweet.find_by_id(params[:id])
+            erb :'tweets/show_tweet'
+        else
+            flash[:message] = "You Must Be Logged In to View a Tweet"
+            redirect to("/login")
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        if logged_in?
+            @tweet = Tweet.find_by_id(params[:id])
+            if @tweet.user_id == current_user.id
+                erb :"tweets/edit_tweet"
+            else
+                flash[:message] = "You Can Only Edit Your Own Tweets"
+                redirect to("/tweets")
+            end 
+        else
+            flash[:message] = "You Must Be Logged In to Edit a Tweet"
+            redirect to("/login")
+        end
+    end
+
+    patch '/tweets/:id' do
+        @tweet = Tweet.find_by_id(params[:id])
+        if params[:content].empty?
+            flash[:message] = "You Cannot Create a Blank Tweet"
+            redirect to("/tweets/#{@tweet.id}/edit")
+        else
+            @tweet.content = params[:content]
+        end
+        @tweet.save
+        redirect to("/tweets/#{@tweet.id}")
+    end
+
+    delete '/tweets/:id/delete' do
+        if logged_in?
+            @tweet = Tweet.find_by_id(params[:id])
+            if @tweet.user_id == current_user.id
+                Tweet.find(@tweet.id).destroy
+                flash[:message] = "Tweet Deleted"
+                redirect to("/tweets")
+            else
+                flash[:message] = "You Can Only Delete Your Own Tweets"
+                redirect to("/tweets")
+            end
+        end
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,47 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if logged_in?
+            redirect to("/tweets")
+        else
+            erb :'users/create_user'
+        end
+    end
 
+    post '/signup' do
+        if params[:username].empty? || params[:email].empty? || params[:password].empty?
+            redirect to("/signup")
+        else 
+            @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+            session['user_id'] = @user.id
+        end
+
+        @user.save
+        redirect to("/tweets")
+    end
+  
+    get '/login' do
+        if logged_in?
+            redirect to("/tweets")
+        else
+            erb :'users/login'
+        end
+    end
+
+    post '/login' do
+        @user = User.find_by(:username => params[:username])
+        session['user_id'] = @user.id
+        redirect to("/tweets")
+    end
+
+    get '/logout' do
+        session.clear
+        redirect to('/login')
+    end
+
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        @tweets = Tweet.all
+        erb :'users/show'
+    end
 end
