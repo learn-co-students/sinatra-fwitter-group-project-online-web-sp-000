064ef857f7a5ab33c3e686221c7160f3cc7b6834
A.J. Romaniello <ajiellodev@gmail.com>
2021-02-24 16:32:01 -0700

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def self.find_by_slug(slug)
+    all.detect { |s| s.slug == slug }
+  end
+
+  def slug
+    username.downcase.gsub(" ", "-")
+  end
+
 end
