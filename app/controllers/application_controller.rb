d5d8433efd857445601e5e3774a047e184054520
A.J. Romaniello <ajiellodev@gmail.com>
2021-02-24 18:52:19 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,44 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "secret"
+    set :session_secret, 'secret'
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def logout
+      session.clear
+    end
+
+  end
+
+  get '/' do
+    erb :home
+  end
+
+  get '/signup' do
+    redirect '/tweets' if logged_in?
+
+    erb :signup
+  end
+
+  post '/signup' do
+    username = params[:username]
+    email = params[:email]
+    password = params[:password]
+
+    redirect '/signup' if username.empty? || email.empty? || password.empty?
+
+    @user = User.create(username: username, email: email, password: password)
+    @user.save
+    session[:user_id] = @user.id
+    redirect '/tweets'
+  end
 end
