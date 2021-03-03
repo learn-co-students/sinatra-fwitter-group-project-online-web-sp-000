b0b7d861303f86cab5c36ea87af786a187c14362
github username <github email address>
2021-03-02 22:26:38 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
 
+    get "/signup" do
+        erb :"users/create_user"
+    end
+
+    post "/signup" do
+        redirect to "tweets/tweets"
+    end
+
+
 
 end
