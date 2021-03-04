da2197196fa894d85a2d722224e41bd20dbdfbc9
Christerle Nounoute <christerlesn@gmail.com>
2021-03-04 22:13:24 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,8 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      User.find(session[:user_id])
+      @current_user ||=
+      User.find_by(id: session[:user_id]) if session[:user_id]
     end
   end
 
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,13 +2,46 @@ class TweetsController < ApplicationController
 
   get '/tweets' do
     if logged_in?
-    # @user = User.find_by(params[:id]) (username: params[:username])
-    erb :'tweets/tweets'
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
     else
       redirect '/login'
     end
   end
 
+  post '/tweets' do
+    if logged_in?
+      if params[:content] ==""
+        redirect to "/tweets/new"
+      else
+        @user = User.find_by_id(session[:user_id])
+        @tweet = current_user.tweets.build(content: params[:content])
+          if @tweet.save
+            redirect to "/tweets/#{@tweet.id}"
+          else
+            redirect to "/tweets/new"
+          end
+        end
+      else
+        redirect to "/login"
+      end
+      end
+  end
+
+
+
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
+
+  # post '/tweets' do
+  #   tweet = Tweet.new(:content => params[:content]) #(:user_id => params[:user_id])
+  #   if tweet.save
+  #     erb :'tweets/tweets'
+  #   else
+  #     redirect '/tweets/new'
+  #   end
+  # end
   # post '/tweets' do
   #   @user = User.find_by(:username => params[:username])
   #
