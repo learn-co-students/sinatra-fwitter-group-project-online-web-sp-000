df3df5965d552fb1c0bf6358fe142ef451fd85ab
Erin Stansbury <ekstansbury@gmail.com>
2021-03-05 05:29:41 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,77 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
+  enable :sessions
+  use Rack::Flash
   configure do
+    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    end
+
+    erb :"/users/create_user"
+  end
+
+  post '/signup' do
+    params.each do |label, input|
+      if input.empty?
+        flash[:new_user_error] = "Please enter a value for #{label}"
+        redirect to '/signup'
+      end
+    end
+
+    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+    session[:user_id] = user.id
+
+    redirect to '/tweets'
+  end
+
+  get '/login' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    end
+
+    erb :"/users/login"
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params["username"])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      flash[:login_error] = "Incorrect login. Please try again."
+      redirect to '/login'
+    end
+  end
+
+
+  get '/logout' do
+    if session[:user_id]
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
+
+  post '/logout' do
+    if session[:user_id]
+      redirect '/tweets'
+    end
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
 
+  get '/users' do
+    if session[:user_id]
+      @users = User.all
+      erb :'users/show'
+    else
+      redirect '/login'
+    end
+  end
+
+  #route to get user page by id
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
 
 end
