63a8e487ec14847e6661da3e3a096b22ccdb98a2
Taylor Treece <taylor.treece@protonmail.com>
2021-03-24 08:46:02 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
   end
 
   get '/' do
@@ -12,11 +13,97 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/login' do
-    erb :'/users/login'
+    if logged_in? 
+      redirect :'/tweets'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    user=User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect :"/tweets"
+    else
+      redirect :'/' 
+    end
   end
 
   get '/signup' do
-    erb :signup
+    if logged_in?
+      redirect :'/tweets'
+    else
+      erb :'/users/create_user'
+    end
+  end
+
+  post '/signup' do
+    user=User.create(params)
+    if user.save
+      session[:user_id]=user.id
+      redirect :'/tweets'
+    else
+      redirect :'/signup'
+    end
+  end
+
+  get '/tweets' do
+    if logged_in?
+    @tweets = Tweet.all
+    @user=current_user
+    erb :'/tweets/index'
+    else
+      redirect :'/login'
+    end
+  end
+
+  get '/users/:user' do
+    @user=User.find_by_slug(username: params[:user])
+    erb :'/user/show'
+  end
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      redirect :'/'
+    end
+  end
+
+  #post '/tweets' do
+    
+  
+  get '/logout' do
+    if logged_in?
+      logout
+      redirect :'/login'
+    else
+      redirect :'/login'
+    end
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def logout
+      session.clear
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def slug
+      self.username.gsub(" ", "-")
+    end
+  
+    def reverse_slug_name(name)
+      name.gsub("-", " ")
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
+
+  def slug
+    self.username.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(name)
+    self.find_by(username: name.gsub("-", " "))
+  end
+
 end
