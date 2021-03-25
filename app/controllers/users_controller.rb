2026f17306dbbe8b55259eb07b4137c2e578e1f8
danny galimi <dannygalimi13@aim.com>
2021-03-25 17:16:22 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        erb :'users/show'
+      end
+
     get '/signup' do
         if !logged_in?
           erb :'users/login', locals: {message: "Please sign up before you sign in"}
@@ -36,5 +41,15 @@ class UsersController < ApplicationController
           redirect to '/signup'
         end
       end
+
+      get '/logout' do
+        if logged_in?
+          session.destroy
+          redirect to '/login'
+        else
+          redirect to '/'
+        end
+      end
+
 
 end
