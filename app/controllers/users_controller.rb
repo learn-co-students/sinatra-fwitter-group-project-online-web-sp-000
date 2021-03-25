f6a1f99e706310a0a101c91878f764303e601f0e
danny galimi <dannygalimi13@aim.com>
2021-03-25 16:39:31 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,22 @@ class UsersController < ApplicationController
         end
       end
 
-      
+      get '/login' do
+        if !logged_in?
+          erb :'users/login'
+        else
+          redirect to '/tweets'
+        end
+      end
+    
+      post '/login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect to "/tweets"
+        else
+          redirect to '/signup'
+        end
+      end
 
 end
