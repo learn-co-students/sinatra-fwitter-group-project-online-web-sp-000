424e667c9f8dd5b214060f58569c882059d342f2
danny galimi <dannygalimi13@aim.com>
2021-03-25 16:18:58 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
+    get '/tweets' do
+        if logged_in?
+          @tweets = Tweet.all
+          erb :'tweets/tweets'
+        else
+          redirect to '/login'
+        end
+      end
 
 
 end
