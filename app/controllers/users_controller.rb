5088e9cd2901eff98a586d6cd1b03c179b6352a8
DabooUA <sbardysh@tutanota.com>
2021-03-28 13:30:16 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
 
+    configure do
+        enable :session
+        set :session_secret, "secret"
+    end
+
+    get "/signup" do
+        if logged_in?
+            erb :'users/create_user' ,locals: {message: "New users need to create account before sign in"}
+        else
+            redirect to "/tweets"
+        end
+
+    end
+
+    post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect to "/signup"
+        else
+            @user = User.new(:username => params[:username], :email => params[:email], password => params[:password])
+            @user.save
+            session[:user_id] = user.id
+            redirect to "/tweets"
+        end
+    end
+
 
 end
