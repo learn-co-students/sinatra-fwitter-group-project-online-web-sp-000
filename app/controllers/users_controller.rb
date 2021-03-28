79fa89f89614fe059b9abfc29ae70d6ac14f124d
DabooUA <sbardysh@tutanota.com>
2021-03-28 13:38:55 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,5 +25,37 @@ class UsersController < ApplicationController
         end
     end
 
+    get '/loging' do
+        if logged_in?
+            erb :'users/login'
+        else
+            redirect to "/tweets"
+        end
+    end
+
+    post "/login" do
+        user = User.find_by(:username => params[:username])
+            if user && user.authenticate(params[:password])
+                session[:user_id] = user.id
+                redirect to "/tweets"
+            else
+                redirect to "/signup"
+            end
+        
+    end
+
+    get "/logout" do
+        if logged_in?
+            redirect to "/"
+        else
+            session.clear
+            redirect to "/login"
+        end
+    end
+
+    get "users/:slug" do
+        @user = User.find_by_slug(params[:slug])
+        erb :"/users/show"
+    end
 
 end
