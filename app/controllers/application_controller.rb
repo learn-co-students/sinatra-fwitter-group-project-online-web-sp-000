ca012e372fbb9833470d7613bb54b7745bc994c0
Ben Hayes <b.t.hayes@comcast.net>
2021-04-13 15:58:35 -0500

    Updatedd tweets controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,18 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,23 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if logged_in?
+      erb :"/tweets/tweets"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/tweets/new' do
+    erb :"tweets/new"
+  end
+
+  post '/tweets' do
+
+    redirect "/tweets/:id"
+  end
+
+  get '/tweets/:id' do
+  end 
+
 end
