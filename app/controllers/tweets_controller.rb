33024a816157767d67256f4952d1ce675150c673
Ben Hayes <b.t.hayes@comcast.net>
2021-04-13 18:05:16 -0500

    Created controller actions for user and tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,12 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-    if logged_in?
+    #if logged_in?
+      @tweets = Tweet.all
       erb :"/tweets/tweets"
-    else
-      redirect "/login"
-    end
+    #else
+    #  redirect "/login"
+    #end
   end
 
   get '/tweets/new' do
@@ -13,11 +14,30 @@ class TweetsController < ApplicationController
   end
 
   post '/tweets' do
-
-    redirect "/tweets/:id"
+    tweet = current_user.tweets.create(content: params[:content])
+    redirect "/tweets/#{@tweet.id}"
   end
 
   get '/tweets/:id' do
-  end 
+    @tweet = Tweet.find(params[:id])
+    erb :"tweets/show_tweet"
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+    erb :"tweets/edit_tweet"
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(params[:content])
+    redirect "/tweets/#{@tweet.id}"
+  end
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.delete
+    redirect "/tweets"
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,35 @@
 class UsersController < ApplicationController
+  get '/signup' do
+    erb :"users/create_user"
+  end
+
+  post '/signup' do
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    else
+      redirect "/signup"
+    end
+  end
+
+  get '/login' do
+    erb :"users/login"
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect "/"
+  end
 
 end
