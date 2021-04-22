809a4d01dd2fbe95445f7cdd3d3f514c2d3f2d24
LGmedia954 <LeslieGonzalez@me.com>
2021-04-21 21:13:25 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,36 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
+
+  use Rack::Flash
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
   end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+    def reject
+      redirect to '/' unless logged_in? #unless is the exact opposite of if. It’s a negated if.
+    end
+
+  end
+
 
 end
