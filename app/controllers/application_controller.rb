8fa4809d7b1b1ba0f3cb74595b1ad33c8aa6a3d5
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-05-02 14:59:20 -0400

    Build Application Controller pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,35 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  enable :sessions
+  set :session_secret, "secret_of_evermore"
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
+  get '/' do 
+    erb :home_page
+  end
+
+  get '/login' do 
+    erb :login
+  end
+
+  get '/signup' do 
+    erb :signup
+  end
+
+  post '/' do 
+    user = User.create(params[:user])
+    session[:user_id] = user.id 
+    redirect "/users/#{user.id}"
+    binding.pry
+  end
+
+
 end
+
+
+
