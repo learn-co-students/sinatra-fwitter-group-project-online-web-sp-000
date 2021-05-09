f22309b435f3cd48c672913eb34edf84297c844a
Drew Thompson <drewallynthompson@gmail.com>
2021-05-08 20:00:43 -0600

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_presence_of :username, :email
   has_many :tweets
+
+  def slug
+    slug = self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+      self.all.find {|t| t.slug === slug}
+  end
 end
