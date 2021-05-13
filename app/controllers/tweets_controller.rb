894e03ffb84793e1dc6f842b8baa753e10bed6b3
Jouse then <jthen93@gmail.com>
2021-05-13 04:20:33 +0000

    models and signup controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,17 @@
 class TweetsController < ApplicationController
-
+  configure do
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+  
+  get '/tweets' do
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      erb :'tweets/index'
+    else
+      redirect '/login'
+    end
+  end
+  
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 class UsersController < ApplicationController
 
+  configure do
+    enable :sessions
+    set :session_secret, "password_security"
+  end
 
+  get '/' do
+    if session[:user_id]
+      erb :'tweets'
+    else
+      erb :'homepage'
+    end
+  end
+  
+  get '/login' do
+    erb :'users/login'
+  end
+  
+  get '/signup' do
+    if session[:user_id]
+      redirect '/tweets'
+    else
+      erb :'users/signup'
+    end
+  end
+  
+  post '/signup' do
+    
+    if params[:username] == "" || params[:password] == ""  || 
+      params[:email] == "" 
+      redirect '/signup'
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+    
+      redirect '/tweets'
+    end
+  end
+  
 end
