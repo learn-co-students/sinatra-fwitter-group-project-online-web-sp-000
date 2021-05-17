56be80151d3beb16652cb09569a6ce305a579755
DabooUA <sbardysh@tutanota.com>
2021-05-16 23:06:25 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -43,7 +43,7 @@ class TweetsController < ApplicationController
     get '/tweets/:id' do
         if logged_in?
             @tweet = Tweet.find_by_id(params[:id])
-            erb :'/tweets/show_tweet'
+            erb :'tweets/show_tweet'
         else
             redirect to '/login'
         end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
 
     get "users/:slug" do
         @user = User.find_by_slug(params[:slug])
-        erb :"/users/show"
+        erb :'users/show'
     end
 
 end
