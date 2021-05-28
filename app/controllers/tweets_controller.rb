747f4058f3ebf8d47ab0f048640a1fda823c98e7
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-28 17:59:17 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,33 @@ class TweetsController < ApplicationController
       redirect to '/login'
     end
   end
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to '/tweets/new'
+      else
+        @tweet = current_user.tweets.create(content: params[:content])
+        redirect to "/tweets/#{@tweet.id}"
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show_tweet'
+    else redirect to '/login'
+    end
+  end
 end
