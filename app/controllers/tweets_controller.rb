0c570f2599e5027cc50f1e1cacc4a90d6b373368
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-01 16:15:17 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -37,4 +37,39 @@ class TweetsController < ApplicationController
     else redirect to '/login'
     end
   end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by(id: params[:id])
+      if @tweet && @tweet.user == current_user
+        erb :'/tweets/edit'
+      else
+        redirect to '/tweets'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/tweets/#{params[:id]}/edit"
+      else
+        @tweet = Tweet.find_by(id: params[:id])
+        if @tweet && @tweet.user == current_user
+          if @tweet.update(content: params[:content])
+            redirect to "/tweets/#{@tweet.id}"
+          else
+            redirect to "/tweets/#{@tweet.id}"
+          end
+        else
+          redirect to "/tweets/"
+        end
+      end
+    else
+      redirect to '/login'
+  end
+end
+
 end
