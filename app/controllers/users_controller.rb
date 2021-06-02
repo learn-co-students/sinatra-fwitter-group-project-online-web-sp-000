746901e73001695d3d208482ee49a58117624b98
DabooUA <sbardysh@tutanota.com>
2021-06-01 21:32:05 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
         end
     end
 
-    get "users/:slug" do
+    get "/users/:slug" do
         @user = User.find_by_slug(params[:slug])
         erb :'users/show'
     end
